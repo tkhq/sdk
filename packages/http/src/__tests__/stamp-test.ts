65e8759a3113b9c91c29e4f11f9b4829d15f7d88
Keyan Zhang <keyan@turnkey.io>
2023-04-13 11:44:47 -0700

    Update internal test for `stamp`

--- packages/http/src/__tests__/stamp-test.ts
+++ packages/http/src/__tests__/stamp-test.ts
@@ -6,18 +6,58 @@ import { readFixture } from "../__fixtures__/shared";
 test("sign", async () => {
   const { privateKey, publicKey, pemPublicKey } = await readFixture();
 
+  const content = crypto.randomBytes(16).toString("hex");
+
   const actualStamp = stamp({
-    content: "hello",
+    content,
     privateKey,
     publicKey,
   });
   expect(actualStamp.publicKey).toBe(publicKey);
   expect(actualStamp.scheme).toBe("SIGNATURE_SCHEME_TK_API_P256");
 
-  // We cannot assert against actualStamp.signature directly, because P-256 signatures are not deterministic
-  const verify = crypto.createVerify("SHA256");
-  verify.update("hello");
-  verify.end();
-  const verified = verify.verify(pemPublicKey, actualStamp.signature, "hex");
-  expect(verified).toBe(true);
+  // We can't snapshot `actualStamp.signature` because P-256 signatures are not deterministic
+  expect(
+    assertValidSignature({
+      content,
+      pubKey: pemPublicKey,
+      signature: actualStamp.signature,
+    })
+  ).toBe(true);
+
+  // Sanity check
+  expect(() => {
+    assertValidSignature({
+      content: "something else that wasn't stamped",
+      pubKey: pemPublicKey,
+      signature: actualStamp.signature,
+    });
+  }).toThrow();
 });
+
+function assertValidSignature({
+  content,
+  pubKey,
+  signature,
+}: {
+  content: string;
+  pubKey: string;
+  signature: string;
+}): true {
+  const verifier = crypto.createVerify("SHA256");
+  verifier.update(content);
+  verifier.end();
+
+  if (verifier.verify(pubKey, signature, "hex")) {
+    return true;
+  }
+
+  throw new Error(
+    [
+      `Invalid signature.`,
+      `content: ${JSON.stringify(content)}`,
+      `pubKey: ${JSON.stringify(pubKey)}`,
+      `signature: ${JSON.stringify(signature)}`,
+    ].join("\n")
+  );
+}
