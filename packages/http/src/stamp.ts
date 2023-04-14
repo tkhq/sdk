361d31ccd2c97c50f268f16d5d48e9c2ee985d8b
Keyan Zhang <keyan@turnkey.io>
2023-04-13 21:04:45 -0700

    stamp: sign via `subtle.sign(...)`

--- packages/http/src/stamp.ts
+++ packages/http/src/stamp.ts
@@ -1,4 +1,4 @@
-import * as crypto from "crypto";
+import { webcrypto } from "crypto";
 
 // Specific byte-sequence for curve prime256v1 (DER encoding)
 const PRIVATE_KEY_PREFIX = Buffer.from(
@@ -25,14 +25,14 @@ export async function stamp(input: {
 
 async function importPrivateKey(
   privateKeyHex: string
-): Promise<crypto.webcrypto.CryptoKey> {
+): Promise<webcrypto.CryptoKey> {
   const privateKeyBuffer = Buffer.from(privateKeyHex, "hex");
   const privateKeyPkcs8Der = Buffer.concat([
     PRIVATE_KEY_PREFIX,
     privateKeyBuffer,
   ]);
 
-  return await crypto.webcrypto.subtle.importKey(
+  return await webcrypto.subtle.importKey(
     "pkcs8",
     privateKeyPkcs8Der,
     {
@@ -45,15 +45,93 @@ async function importPrivateKey(
 }
 
 async function signMessage(
-  privateKey: crypto.webcrypto.CryptoKey,
+  privateKey: webcrypto.CryptoKey,
   content: string
 ): Promise<string> {
-  const sign = crypto.createSign("SHA256");
+  const signatureIeee1363 = await webcrypto.subtle.sign(
+    {
+      name: "ECDSA",
+      hash: "SHA-256",
+    },
+    privateKey,
+    Buffer.from(content)
+  );
 
-  sign.write(Buffer.from(content));
-  sign.end();
+  const signatureDer = convertEcdsaIeee1363ToDer(
+    new Uint8Array(signatureIeee1363)
+  );
 
-  const signature = sign.sign(privateKey as any, "hex");
+  return Buffer.from(signatureDer).toString("hex");
+}
 
-  return signature;
+/**
+ * `SubtleCrypto.sign(...)` outputs signature in IEEE P1363 format:
+ * - https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/sign#ecdsa
+ *
+ * Turnkey expects the signature encoding to be DER-encoded ASN.1:
+ * - https://github.com/tkhq/tkcli/blob/7f0159af5a73387ff050647180d1db4d3a3aa033/src/internal/apikey/apikey.go#L149
+ *
+ * Code modified from https://github.com/google/tink/blob/6f74b99a2bfe6677e3670799116a57268fd067fa/javascript/subtle/elliptic_curves.ts#L114
+ *
+ * Transform an ECDSA signature in IEEE 1363 encoding to DER encoding.
+ *
+ * @param ieee the ECDSA signature in IEEE encoding
+ * @return ECDSA signature in DER encoding
+ */
+function convertEcdsaIeee1363ToDer(ieee: Uint8Array): Uint8Array {
+  if (ieee.length % 2 != 0 || ieee.length == 0 || ieee.length > 132) {
+    throw new Error(
+      "Invalid IEEE P1363 signature encoding. Length: " + ieee.length
+    );
+  }
+  const r = toUnsignedBigNum(ieee.subarray(0, ieee.length / 2));
+  const s = toUnsignedBigNum(ieee.subarray(ieee.length / 2, ieee.length));
+  let offset = 0;
+  const length = 1 + 1 + r.length + 1 + 1 + s.length;
+  let der;
+  if (length >= 128) {
+    der = new Uint8Array(length + 3);
+    der[offset++] = 48;
+    der[offset++] = 128 + 1;
+    der[offset++] = length;
+  } else {
+    der = new Uint8Array(length + 2);
+    der[offset++] = 48;
+    der[offset++] = length;
+  }
+  der[offset++] = 2;
+  der[offset++] = r.length;
+  der.set(r, offset);
+  offset += r.length;
+  der[offset++] = 2;
+  der[offset++] = s.length;
+  der.set(s, offset);
+  return der;
+}
+
+/**
+ * Code modified from https://github.com/google/tink/blob/6f74b99a2bfe6677e3670799116a57268fd067fa/javascript/subtle/elliptic_curves.ts#L311
+ *
+ * Transform a big integer in big endian to minimal unsigned form which has
+ * no extra zero at the beginning except when the highest bit is set.
+ */
+function toUnsignedBigNum(bytes: Uint8Array): Uint8Array {
+  // Remove zero prefixes.
+  let start = 0;
+  while (start < bytes.length && bytes[start] == 0) {
+    start++;
+  }
+  if (start == bytes.length) {
+    start = bytes.length - 1;
+  }
+  let extraZero = 0;
+
+  // If the 1st bit is not zero, add 1 zero byte.
+  if ((bytes[start]! & 128) == 128) {
+    // Add extra zero.
+    extraZero = 1;
+  }
+  const res = new Uint8Array(bytes.length - start + extraZero);
+  res.set(bytes.subarray(start), extraZero);
+  return res;
 }
