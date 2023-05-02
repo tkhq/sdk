7b0601b13b3d0e45d8bd7c9ec2ba06a730cefc11
Keyan Zhang <keyan@turnkey.io>
2023-05-02 14:06:12 -0700

    Refactor tests

--- packages/ethers/src/__tests__/index-test.ts
+++ packages/ethers/src/__tests__/index-test.ts
@@ -1,75 +1,79 @@
 import { ethers } from "ethers";
-import { test, expect } from "@jest/globals";
+import { test, expect, beforeEach, describe } from "@jest/globals";
 import { TurnkeySigner, TurnkeyActivityError } from "../";
 
-test("TurnkeySigner", async () => {
+// @ts-expect-error
+const testCase: typeof test = (...argList) => {
   if (!process.env.BANNED_TO_ADDRESS) {
     // For now, this test requires certain environment variables to be injected (from Turnkey's internal environment)
-    return;
+    return test.skip(...argList);
   }
 
-  const apiPublicKey = assertNonEmptyString(
-    process.env.API_PUBLIC_KEY,
-    `process.env.API_PUBLIC_KEY`
-  );
-  const apiPrivateKey = assertNonEmptyString(
-    process.env.API_PRIVATE_KEY,
-    `process.env.API_PRIVATE_KEY`
-  );
-  const baseUrl = assertNonEmptyString(
-    process.env.BASE_URL,
-    `process.env.BASE_URL`
-  );
-  const organizationId = assertNonEmptyString(
-    process.env.ORGANIZATION_ID,
-    `process.env.ORGANIZATION_ID`
-  );
-  const privateKeyId = assertNonEmptyString(
-    process.env.PRIVATE_KEY_ID,
-    `process.env.PRIVATE_KEY_ID`
-  );
-  const expectedEthAddress = assertNonEmptyString(
-    process.env.EXPECTED_ETH_ADDRESS,
-    `process.env.EXPECTED_ETH_ADDRESS`
-  );
-  const bannedToAddress = assertNonEmptyString(
-    process.env.BANNED_TO_ADDRESS,
-    `process.env.BANNED_TO_ADDRESS`
-  );
+  return test(...argList);
+};
 
-  const provider = new ethers.providers.InfuraProvider("goerli");
+describe("TurnkeySigner", () => {
+  let connectedSigner: TurnkeySigner;
+  let chainId: number;
+  let expectedEthAddress: string;
+  let bannedToAddress: string;
 
-  const signer = new TurnkeySigner({
-    apiPublicKey,
-    apiPrivateKey,
-    baseUrl,
-    organizationId,
-    privateKeyId,
-  }).connect(provider);
+  beforeEach(async () => {
+    if (!process.env.BANNED_TO_ADDRESS) {
+      // For now, this test requires certain environment variables to be injected (from Turnkey's internal environment)
+      return;
+    }
 
-  expect(ethers.Signer.isSigner(signer)).toBe(true);
+    const apiPublicKey = assertNonEmptyString(
+      process.env.API_PUBLIC_KEY,
+      `process.env.API_PUBLIC_KEY`
+    );
+    const apiPrivateKey = assertNonEmptyString(
+      process.env.API_PRIVATE_KEY,
+      `process.env.API_PRIVATE_KEY`
+    );
+    const baseUrl = assertNonEmptyString(
+      process.env.BASE_URL,
+      `process.env.BASE_URL`
+    );
+    const organizationId = assertNonEmptyString(
+      process.env.ORGANIZATION_ID,
+      `process.env.ORGANIZATION_ID`
+    );
+    const privateKeyId = assertNonEmptyString(
+      process.env.PRIVATE_KEY_ID,
+      `process.env.PRIVATE_KEY_ID`
+    );
 
-  expect(await signer.getAddress()).toBe(expectedEthAddress);
+    expectedEthAddress = assertNonEmptyString(
+      process.env.EXPECTED_ETH_ADDRESS,
+      `process.env.EXPECTED_ETH_ADDRESS`
+    );
 
-  const chainId = (await provider.getNetwork()).chainId;
+    bannedToAddress = assertNonEmptyString(
+      process.env.BANNED_TO_ADDRESS,
+      `process.env.BANNED_TO_ADDRESS`
+    );
 
-  // Test transaction signing
-  const tx = await signer.signTransaction({
-    to: "0x2Ad9eA1E677949a536A270CEC812D6e868C88108",
-    value: ethers.utils.parseEther("1.0"),
-    chainId,
-    nonce: 0,
-    gasLimit: 21000,
-    maxFeePerGas: 2e9,
-    maxPriorityFeePerGas: 200e9,
-    type: 2,
+    const provider = new ethers.providers.InfuraProvider("goerli");
+
+    connectedSigner = new TurnkeySigner({
+      apiPublicKey,
+      apiPrivateKey,
+      baseUrl,
+      organizationId,
+      privateKeyId,
+    }).connect(provider);
+
+    chainId = (await connectedSigner.provider!.getNetwork()).chainId;
   });
 
-  expect(tx).toMatch(/^0x/);
+  testCase("it signs transactions", async () => {
+    expect(ethers.Signer.isSigner(connectedSigner)).toBe(true);
+    expect(await connectedSigner.getAddress()).toBe(expectedEthAddress);
 
-  try {
-    await signer.signTransaction({
-      to: bannedToAddress,
+    const tx = await connectedSigner.signTransaction({
+      to: "0x2Ad9eA1E677949a536A270CEC812D6e868C88108",
       value: ethers.utils.parseEther("1.0"),
       chainId,
       nonce: 0,
@@ -78,74 +82,98 @@ test("TurnkeySigner", async () => {
       maxPriorityFeePerGas: 200e9,
       type: 2,
     });
-  } catch (error) {
-    expect(error).toBeInstanceOf(TurnkeyActivityError);
 
-    const { message, cause, activityId, activityStatus, activityType } =
-      error as TurnkeyActivityError;
+    expect(tx).toMatch(/^0x/);
+  });
 
-    // We don't have activity info here, because signer returns an http error
-    expect({
-      message,
-      cause,
-      activityId,
-      activityStatus,
-      activityType,
-    }).toMatchInlineSnapshot(`
-      {
-        "activityId": null,
-        "activityStatus": null,
-        "activityType": null,
-        "cause": [Error: 500: Internal Server Error | Internal error 2: unable to execute ruling: ump denied request explicitly],
-        "message": "Failed to sign",
+  testCase(
+    "it throws when there's a deny policy on the transaction",
+    async () => {
+      try {
+        await connectedSigner.signTransaction({
+          to: bannedToAddress,
+          value: ethers.utils.parseEther("1.0"),
+          chainId,
+          nonce: 0,
+          gasLimit: 21000,
+          maxFeePerGas: 2e9,
+          maxPriorityFeePerGas: 200e9,
+          type: 2,
+        });
+      } catch (error) {
+        expect(error).toBeInstanceOf(TurnkeyActivityError);
+
+        const { message, cause, activityId, activityStatus, activityType } =
+          error as TurnkeyActivityError;
+
+        // We don't have activity info here, because signer returns an http error
+        expect({
+          message,
+          cause,
+          activityId,
+          activityStatus,
+          activityType,
+        }).toMatchInlineSnapshot(`
+        {
+          "activityId": null,
+          "activityStatus": null,
+          "activityType": null,
+          "cause": [Error: 500: Internal Server Error | Internal error 2: unable to execute ruling: ump denied request explicitly],
+          "message": "Failed to sign",
+        }
+      `);
       }
-    `);
-  }
-
-  // Test message (raw payload) signing, `eth_sign` style
-  const message = "Hello Turnkey";
-  const signMessageSignature = await signer.signMessage(message);
-
-  expect(signMessageSignature).toMatch(/^0x/);
-  expect(ethers.utils.verifyMessage(message, signMessageSignature)).toEqual(
-    expectedEthAddress
+    }
   );
 
-  // Test typed data signing (EIP-712)
-  const typedData = {
-    types: {
-      // Note that we do not need to include `EIP712Domain` as a type here, as Ethers will automatically inject it for us
-      Person: [
-        { name: "name", type: "string" },
-        { name: "wallet", type: "address" },
-      ],
-    },
-    domain: {
-      name: "EIP712 Test",
-      version: "1",
-    },
-    primaryType: "Person",
-    message: {
-      name: "Alice",
-      wallet: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e",
-    },
-  };
+  testCase("it signs messages, `eth_sign` style", async () => {
+    // Test message (raw payload) signing, `eth_sign` style
+    const message = "Hello Turnkey";
+    const signMessageSignature = await connectedSigner.signMessage(message);
 
-  const signTypedDataSignature = await signer.signTypedData(
-    typedData.domain,
-    typedData.types,
-    typedData.message
-  );
+    expect(signMessageSignature).toMatch(/^0x/);
+    expect(ethers.utils.verifyMessage(message, signMessageSignature)).toEqual(
+      expectedEthAddress
+    );
+  });
 
-  expect(signTypedDataSignature).toMatch(/^0x/);
-  expect(
-    ethers.utils.verifyTypedData(
+  testCase("it signs typed data (EIP-712)", async () => {
+    // Test typed data signing (EIP-712)
+    const typedData = {
+      types: {
+        // Note that we do not need to include `EIP712Domain` as a type here, as Ethers will automatically inject it for us
+        Person: [
+          { name: "name", type: "string" },
+          { name: "wallet", type: "address" },
+        ],
+      },
+      domain: {
+        name: "EIP712 Test",
+        version: "1",
+      },
+      primaryType: "Person",
+      message: {
+        name: "Alice",
+        wallet: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e",
+      },
+    };
+
+    const signTypedDataSignature = await connectedSigner.signTypedData(
       typedData.domain,
       typedData.types,
-      typedData.message,
-      signTypedDataSignature
-    )
-  ).toEqual(expectedEthAddress);
+      typedData.message
+    );
+
+    expect(signTypedDataSignature).toMatch(/^0x/);
+    expect(
+      ethers.utils.verifyTypedData(
+        typedData.domain,
+        typedData.types,
+        typedData.message,
+        signTypedDataSignature
+      )
+    ).toEqual(expectedEthAddress);
+  });
 });
 
 function assertNonEmptyString(input: unknown, name: string): string {
