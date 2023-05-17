7c61bb3363a4d31a5bd96c9c70980d0219aba519
Andrew Min <andrew@turnkey.io>
2023-05-17 19:03:32 -0400

    change env load location

--- examples/with-uniswap/src/provider.ts
+++ examples/with-uniswap/src/provider.ts
@@ -1,3 +1,11 @@
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+// Load environment variables from `.env.local`
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+
+console.log('after', process.env)
+
 import { ethers } from "ethers";
 import { TurnkeySigner } from "@turnkey/ethers";
 import { Environment } from "./constants";
--- examples/with-uniswap/src/scripts/main.ts
+++ examples/with-uniswap/src/scripts/main.ts
@@ -1,9 +1,3 @@
-import * as dotenv from "dotenv";
-import * as path from "path";
-
-// Load environment variables from `.env.local`
-dotenv.config({ path: path.resolve(process.cwd(), "./.env.local") });
-
 import { ethers } from "ethers";
 import { createV3Trade, executeTrade } from "../base";
 import { getProvider, getTurnkeySigner } from "../provider";
