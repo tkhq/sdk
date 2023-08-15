13ebea9d7375633b50724cb6cc61af19961680a2
Arnaud Brousseau <rno@turnkey.io>
2023-08-15 11:55:45 -0500

    Move signature tests to api-key-stamper

--- packages/api-key-stamper/jest.config.js
+++ packages/api-key-stamper/jest.config.js
@@ -3,7 +3,7 @@ const config = {
   transform: {
     "\\.[jt]sx?$": "@turnkey/jest-config/transformer.js",
   },
-  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/", "<rootDir>/src/__tests__/shared.ts"],
 };
 
 module.exports = config;
--- packages/api-key-stamper/src/__tests__/signature-test.ts
+++ packages/api-key-stamper/src/__tests__/signature-test.ts
@@ -1,8 +1,8 @@
 import * as crypto from "crypto";
 import { test, expect, beforeAll } from "@jest/globals";
-import { stamp as stampUniversal } from "../universal";
-import { stamp as stampNode } from "../stamp.node";
-import { stamp as stampWebCrypto } from "../stamp.webcrypto";
+import { signWithApiKey as signUniversal } from "../index";
+import { signWithApiKey as signNode } from "../nodecrypto";
+import { signWithApiKey as signWeb } from "../webcrypto";
 
 import { readFixture } from "../__fixtures__/shared";
 import { generateKeyPairWithOpenSsl } from "./shared";
@@ -13,28 +13,26 @@ beforeAll(() => {
 });
 
 test.each([
-  { impl: stampNode, name: "stamp (node)" },
-  { impl: stampWebCrypto, name: "stamp (WebCrypto)" },
-  { impl: stampUniversal, name: "stamp (universal)" },
-])("sign with Turnkey fixture: $name", async ({ impl: stamp }) => {
+  { impl: signNode, name: "sign (node crypto)" },
+  { impl: signWeb, name: "sign (WebCrypto)" },
+  { impl: signUniversal, name: "sign (universal)" },
+])("sign with Turnkey fixture: $name", async ({ impl: sign }) => {
   const { privateKey, publicKey, pemPublicKey } = await readFixture();
 
   const content = crypto.randomBytes(16).toString("hex");
 
-  const actualStamp = await stamp({
+  const signature = await sign({
     content,
     privateKey,
     publicKey,
   });
-  expect(actualStamp.publicKey).toBe(publicKey);
-  expect(actualStamp.scheme).toBe("SIGNATURE_SCHEME_TK_API_P256");
 
   // We can't snapshot `actualStamp.signature` because P-256 signatures are not deterministic
   expect(
     assertValidSignature({
       content,
       pemPublicKey,
-      signature: actualStamp.signature,
+      signature: signature,
     })
   ).toBe(true);
 
@@ -43,15 +41,15 @@ test.each([
     assertValidSignature({
       content: "something else that wasn't stamped",
       pemPublicKey,
-      signature: actualStamp.signature,
+      signature: signature,
     });
   }).toThrow();
 });
 
 test.each([
-  { impl: stampNode, name: "stamp (node)" },
-  { impl: stampWebCrypto, name: "stamp (WebCrypto)" },
-  { impl: stampUniversal, name: "stamp (universal)" },
+  { impl: signNode, name: "stamp (node)" },
+  { impl: signWeb, name: "stamp (WebCrypto)" },
+  { impl: signUniversal, name: "stamp (universal)" },
 ])("sign with openssl generated key pairs: $name", async ({ impl: stamp }) => {
   // Run 20 times, where each run spawns 10 keys in parallel -> 200 tests in total
   for (let i = 0; i < 20; i++) {
@@ -65,20 +63,18 @@ test.each([
           return String.fromCharCode(Math.floor(Math.random() * 65536));
         }).join("");
 
-        const actualStamp = await stamp({
+        const signature = await stamp({
           content,
           privateKey,
           publicKey,
         });
-        expect(actualStamp.publicKey).toBe(publicKey);
-        expect(actualStamp.scheme).toBe("SIGNATURE_SCHEME_TK_API_P256");
 
         // We can't snapshot `actualStamp.signature` because P-256 signatures are not deterministic
         expect(
           assertValidSignature({
             content,
             pemPublicKey,
-            signature: actualStamp.signature,
+            signature: signature,
           })
         ).toBe(true);
 
@@ -87,7 +83,7 @@ test.each([
           assertValidSignature({
             content: "something else that wasn't stamped",
             pemPublicKey,
-            signature: actualStamp.signature,
+            signature: signature,
           });
         }).toThrow();
       })
