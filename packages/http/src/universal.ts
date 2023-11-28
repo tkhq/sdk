071bef5cf84d042f8c2bae4e4a2fe8ca80e4d20c
Arnaud Brousseau <rno@turnkey.io>
2023-11-28 11:13:52 -0600

    Stop using require in universal files

--- packages/http/src/universal.ts
+++ packages/http/src/universal.ts
@@ -1,10 +1,11 @@
 /// <reference lib="dom" />
+import { fetch as xFetch } from 'cross-fetch';
 let fetch: typeof globalThis.fetch;
 
 if (typeof globalThis?.fetch !== "undefined") {
   fetch = globalThis.fetch;
 } else {
-  fetch = require("cross-fetch");
+  fetch = xFetch;
 }
 
 export { fetch };
