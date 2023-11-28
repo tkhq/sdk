0f06c5945ac4622d3ac3d3623151d42b365773b3
Arnaud Brousseau <rno@turnkey.io>
2023-11-28 11:22:01 -0600

    Prettier

--- packages/http/src/universal.ts
+++ packages/http/src/universal.ts
@@ -1,5 +1,5 @@
 /// <reference lib="dom" />
-import { fetch as xFetch } from 'cross-fetch';
+import { fetch as xFetch } from "cross-fetch";
 let fetch: typeof globalThis.fetch;
 
 if (typeof globalThis?.fetch !== "undefined") {
