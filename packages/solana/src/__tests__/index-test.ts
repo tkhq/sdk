ed1fca1c195c6f57a1f1965300779be24e06ca38
Montana Wong <montana@dynamic.xyz>
2024-01-22 17:28:13 -0500

    implement signMessage on Solana TurnkeySigner

--- packages/solana/src/__tests__/index-test.ts
+++ packages/solana/src/__tests__/index-test.ts
@@ -5,6 +5,9 @@ import { ApiKeyStamper } from "@turnkey/api-key-stamper";
 import { PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 
 describe("TurnkeySigner", () => {
+  const organizationId = "4456e4c2-e8b5-4b93-a0cf-1dfae265c12c";
+  const apiPublicKey = "025d374c674fc389c761462f3c59c0acabdcb3a17c599d9e62e5fe78fe984cfbeb";
+  const turnkeySolAddress = 'D8P541wwnertZTgDT14kYJPoFT2eHUFqjTgPxMK5qatM';
   test("can sign a Solana transfer against production", async () => {
     if (!process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY) {
       // This test requires an env var to be set
@@ -17,18 +20,15 @@ describe("TurnkeySigner", () => {
     const client = new TurnkeyClient(
       { baseUrl: "https://api.turnkey.com" },
       new ApiKeyStamper({
-        apiPublicKey:
-          "025d374c674fc389c761462f3c59c0acabdcb3a17c599d9e62e5fe78fe984cfbeb",
+        apiPublicKey,
         apiPrivateKey: process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY,
       })
     );
 
     const signer = new TurnkeySigner({
-      organizationId: "4456e4c2-e8b5-4b93-a0cf-1dfae265c12c",
+      organizationId,
       client,
     });
-
-    const turnkeySolAddress = "D8P541wwnertZTgDT14kYJPoFT2eHUFqjTgPxMK5qatM";
 
     const transferTransaction = new Transaction().add(
       SystemProgram.transfer({
@@ -48,5 +48,31 @@ describe("TurnkeySigner", () => {
     expect(transferTransaction.signatures.length).toBe(0);
     await signer.addSignature(transferTransaction, turnkeySolAddress);
     expect(transferTransaction.signatures.length).toBe(1);
+  });
+
+  test("can sign a message with a Solana account", async () => {
+    if (!process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY) {
+      // This test requires an env var to be set
+      console.warn(
+          "This test is skipped because it cannot run without SOLANA_TEST_ORG_API_PRIVATE_KEY set"
+      );
+      return;
+    }
+
+    const client = new TurnkeyClient(
+        { baseUrl: "https://api.turnkey.com" },
+        new ApiKeyStamper({
+          apiPublicKey,
+          apiPrivateKey: process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY,
+        })
+    );
+
+    const signer = new TurnkeySigner({
+      organizationId,
+      client,
+    });
+
+    const signature = await signer.signMessage('Hello world', turnkeySolAddress);
+    expect(signature).toBeDefined();
   });
 });
--- packages/solana/src/index.ts
+++ packages/solana/src/index.ts
@@ -1,5 +1,6 @@
 import { PublicKey, type Transaction } from "@solana/web3.js";
 import { TurnkeyActivityError, TurnkeyClient } from "@turnkey/http";
+import type {definitions} from "@turnkey/http/dist/__generated__/services/coordinator/public/v1/public_api.types";
 
 export class TurnkeySigner {
   public readonly organizationId: string;
@@ -20,14 +21,41 @@ export class TurnkeySigner {
     const fromKey = new PublicKey(fromAddress);
     const messageToSign = tx.serializeMessage();
 
+    const signRawPayloadResult = await this.signRawPayload(
+        messageToSign.toString("hex"),
+        "PAYLOAD_ENCODING_HEXADECIMAL",
+        fromAddress,
+    );
+
+    const signature = `${signRawPayloadResult.signRawPayloadResult?.r}${signRawPayloadResult.signRawPayloadResult?.s}`;
+
+    tx.addSignature(fromKey, Buffer.from(signature, "hex"));
+  }
+
+  /**
+   * This function takes a message and returns it after being signed with Turnkey
+   *
+   * @param message The message to sign (string)
+   * @param fromAddress Solana address (base58 encoded)
+   */
+  public async signMessage(message: string, fromAddress: string): Promise<string> {
+    const signRawPayloadResult = await this.signRawPayload(
+        message,
+        "PAYLOAD_ENCODING_TEXT_UTF8",
+        fromAddress,
+    );
+    return `${signRawPayloadResult.signRawPayloadResult?.r}${signRawPayloadResult.signRawPayloadResult?.s}`;
+  }
+
+  private async signRawPayload(payload: string, encoding: definitions["v1PayloadEncoding"], signWith: string) {
     const response = await this.client.signRawPayload({
       type: "ACTIVITY_TYPE_SIGN_RAW_PAYLOAD_V2",
       organizationId: this.organizationId,
       timestampMs: String(Date.now()),
       parameters: {
-        signWith: fromAddress,
-        payload: messageToSign.toString("hex"),
-        encoding: "PAYLOAD_ENCODING_HEXADECIMAL",
+        signWith,
+        payload,
+        encoding,
         // Note: unlike ECDSA, EdDSA's API does not support signing raw digests (see RFC 8032).
         // Turnkey's signer requires an explicit value to be passed here to minimize ambiguity.
         hashFunction: "HASH_FUNCTION_NOT_APPLICABLE",
@@ -45,8 +73,6 @@ export class TurnkeySigner {
       });
     }
 
-    const signature = `${result.signRawPayloadResult?.r}${result.signRawPayloadResult?.s}`;
-
-    tx.addSignature(fromKey, Buffer.from(signature, "hex"));
+    return result;
   }
 }
