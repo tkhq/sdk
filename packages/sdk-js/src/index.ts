12701a210718eae47cbb4bca6da3a13f0f23a30a
Jordan Leigh <jordan@turnkey.io>
2024-01-31 12:06:54 -0500

    initial commit bundling js packages into single sdk-js package

---
+++ packages/sdk-js/src/index.ts
@@ -0,0 +1,71 @@
+import {
+  ApiKeyStamper,
+  signWithApiKey,
+  TApiKeyStamperConfig,
+} from "@turnkey/api-key-stamper";
+
+import {
+  createActivityPoller,
+  getWebAuthnAttestation,
+  sealAndStampRequestBody,
+  TSignedRequest,
+  TurnkeyActivityError,
+  TurnkeyApi,
+  TurnkeyApiTypes,
+  TurnkeyClient,
+  TurnkeyRequestError,
+  VERSION
+} from "@turnkey/http";
+
+import {
+  IframeEventType,
+  IframeStamper,
+  TIframeStamperConfig
+} from "@turnkey/iframe-stamper";
+
+import {
+  TWebauthnStamperConfig,
+  WebauthnStamper
+} from "@turnkey/webauthn-stamper";
+
+// Classes
+export {
+  ApiKeyStamper,
+  IframeStamper,
+  TurnkeyActivityError,
+  TurnkeyClient,
+  TurnkeyRequestError,
+  WebauthnStamper
+}
+
+// Types
+export type {
+  TApiKeyStamperConfig,
+  TIframeStamperConfig,
+  TSignedRequest,
+  TurnkeyApiTypes,
+  TWebauthnStamperConfig
+}
+
+// Functions
+export {
+  createActivityPoller,
+  getWebAuthnAttestation,
+  sealAndStampRequestBody,
+  signWithApiKey
+}
+
+// Enums
+export {
+  IframeEventType
+}
+
+// Constants
+export {
+  VERSION
+}
+
+// ???
+export {
+  TurnkeyApi,
+}
