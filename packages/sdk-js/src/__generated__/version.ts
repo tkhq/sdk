a54c0a494cb9258b88a719404cee7094f51e0d48
Jordan Leigh <jordan@turnkey.io>
2024-02-01 16:43:30 -0500

    WIP: updating the Codegen to generate the SDKClient interface

---
+++ packages/sdk-js/src/__generated__/version.ts
@@ -0,0 +1,1 @@
+export const VERSION = "CLIENT_VERSION";
---
+++ packages/sdk-js/src/__types__/base.ts
@@ -0,0 +1,37 @@
+export type GrpcStatus = {
+  message: string;
+  code: number;
+  details: unknown[] | null;
+};
+
+export type TStamp = {
+  stampHeaderName: string;
+  stampHeaderValue: string;
+}
+
+export interface TStamper {
+  stamp: (input: string) => Promise<TStamp>;
+}
+
+export type THttpConfig = {
+  baseUrl: string;
+}
+
+export class TurnkeyRequestError extends Error {
+  details: any[] | null;
+  code: number;
+
+  constructor(input: GrpcStatus) {
+    let turnkeyErrorMessage = `Turnkey error ${input.code}: ${input.message}`;
+
+    if (input.details != null) {
+      turnkeyErrorMessage += ` (Details: ${JSON.stringify(input.details)})`;
+    }
+
+    super(turnkeyErrorMessage);
+
+    this.name = "TurnkeyRequestError";
+    this.details = input.details ?? null;
+    this.code = input.code;
+  }
+}
