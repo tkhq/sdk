d3397b12a0a6d8e444eba52bc6765bf76962f939
Taylor Dawson <taylor@turnkey.io>
2024-03-19 15:43:27 +0000

    adds eth_signTypedData_v4 + tests

--- packages/eip-1193-provider/src/__tests__/index.test.ts
+++ packages/eip-1193-provider/src/__tests__/index.test.ts
@@ -2,27 +2,39 @@ import 'dotenv/config';
 
 import {
   type Address,
-  type EIP1193Provider,
   type Index,
   type Quantity,
-  type RpcTransactionRequest,
   type TransactionRequestEIP1559,
-  createWalletClient,
   getAddress,
-  http,
   numberToHex,
   parseEther,
   parseGwei,
   recoverAddress,
-  serializeTransaction,
   stringToHex,
+  verifyTypedData,
+  ProviderRpcError,
+  EIP1474Methods,
 } from 'viem';
 
 import { sepolia } from 'viem/chains';
 
 import { beforeEach, describe, it, expect } from '@jest/globals';
+
 import { createAPIKeyStamper, createEIP1193Provider } from '../';
 import { TurnkeyClient } from '@turnkey/http';
+import type { UUID } from 'crypto';
+import type { TurnkeyEIP1193Provider } from '../types';
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      WALLET_ID: UUID;
+      ORG_ID: UUID;
+      TURNKEY_API_PUBLIC_KEY: string;
+      TURNKEY_API_PRIVATE_KEY: string;
+    }
+  }
+}
 
 // import { preprocessTransaction } from '../packages/eip-1193-provider/src/utils';
 // import { createAccount } from '@turnkey/viem';
@@ -38,7 +50,7 @@ const RECEIVER_ADDRESS: Address = '0x6f85Eb534E14D605d4e82bF97ddF59c18F686699';
 const TEST_TIMEOUT = 10_000;
 
 describe('Test Turnkey EIP-1193 Provider', () => {
-  let eip1193Provider;
+  let eip1193Provider: TurnkeyEIP1193Provider;
 
   beforeEach(async () => {
     const tk = new TurnkeyClient(
@@ -65,9 +77,10 @@ describe('Test Turnkey EIP-1193 Provider', () => {
           const accounts = await eip1193Provider?.request({
             method: 'eth_accounts',
           });
-          expect(accounts).toBeDefined();
+
+          expect(accounts).not.toBeUndefined();
           expect(Array.isArray(accounts)).toBeTruthy();
-          expect(accounts.length).toBeGreaterThan(0); // Corrected line
+          expect(accounts.length).toBeGreaterThan(0);
           expect(accounts).toContain(EXPECTED_WALLET_ADDRESS);
         });
       });
@@ -77,37 +90,95 @@ describe('Test Turnkey EIP-1193 Provider', () => {
           const accounts = await eip1193Provider.request({
             method: 'eth_requestAccounts',
           });
-
-          expect(accounts).toBeInstanceOf(Array);
-          expect(accounts.length).toBeGreaterThan(0);
+          expect(Array.isArray(accounts)).toBeTruthy();
+          expect(accounts).toHaveLength(1);
           expect(accounts).toContain(EXPECTED_WALLET_ADDRESS);
         });
       });
 
       describe('eth_sign/person_sign', () => {
-        it(
-          'should sign a message',
-          async () => {
-            const messageDigest = stringToHex('A man, a plan, a canal, Panama');
-            const signerAddress = EXPECTED_WALLET_ADDRESS;
-            const signature = await eip1193Provider?.request({
-              method: 'personal_sign',
-              params: [signerAddress, messageDigest],
-            });
-            expect(signature).toBeDefined();
-            expect(signature).not.toBe('');
-            const address = await recoverAddress({
-              hash: messageDigest,
-              signature: signature!,
-            });
-            expect(getAddress(address)).toBe(getAddress(signerAddress));
-            expect(signature).toMatch(/^0x.*$/);
-          },
-          TEST_TIMEOUT
-        );
+        it('should sign a message', async () => {
+          const messageDigest = stringToHex('A man, a plan, a canal, Panama');
+          const signerAddress = EXPECTED_WALLET_ADDRESS;
+          const signature = await eip1193Provider?.request({
+            method: 'personal_sign',
+            params: [signerAddress, messageDigest],
+          });
+          expect(signature).not.toBeUndefined();
+          expect(signature).not.toBe('');
+          const address = await recoverAddress({
+            hash: messageDigest,
+            signature: signature!,
+          });
+          expect(getAddress(address)).toBe(getAddress(signerAddress));
+          expect(signature).toMatch(/^0x.*$/);
+        });
       });
+      describe('eth_signTypedData_v4', () => {
+        it('should sign typed data according to EIP-712', async () => {
+          const address = EXPECTED_WALLET_ADDRESS;
+          // Test typed data signing (EIP-712)
+          // All properties on a domain are optional
+          const domain = {
+            name: 'Ether Mail',
+            version: '1',
+            chainId: 1,
+            verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+          } as const;
 
-      describe.only('eth_signTransaction', () => {
+          // The named list of all type definitions
+          const types = {
+            Person: [
+              { name: 'name', type: 'string' },
+              { name: 'wallet', type: 'address' },
+            ],
+            Mail: [
+              { name: 'from', type: 'Person' },
+              { name: 'to', type: 'Person' },
+              { name: 'contents', type: 'string' },
+            ],
+          } as const;
+          const primaryType = 'Mail';
+          const message = {
+            from: {
+              name: 'Cow',
+              wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+            },
+            to: {
+              name: 'Bob',
+              wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+            },
+            contents: 'Hello, Bob!',
+          } as const;
+          const typedData = {
+            address,
+            domain,
+            types,
+            primaryType,
+            message,
+          } as const;
+
+          const signature = await eip1193Provider?.request({
+            method: 'eth_signTypedData_v4',
+            params: [EXPECTED_WALLET_ADDRESS, typedData],
+          });
+
+          const valid = await verifyTypedData({
+            address: EXPECTED_WALLET_ADDRESS,
+            domain,
+            types,
+            primaryType,
+            message,
+            signature,
+          });
+
+          expect(valid).toBeTruthy();
+          expect(signature).not.toBeNull();
+          expect(signature).not.toBe('');
+          expect(signature).toMatch(/^0x[a-fA-F0-9]+$/);
+        });
+      });
+      describe('eth_signTransaction', () => {
         it('should sign a transaction', async () => {
           const from = EXPECTED_WALLET_ADDRESS;
           const to = RECEIVER_ADDRESS;
@@ -140,10 +211,10 @@ describe('Test Turnkey EIP-1193 Provider', () => {
               } as TransactionRequestEIP1559<Quantity, Index, '0x2'>,
             ],
           });
-
+          console.log(signature);
           expect(signature).toBeDefined();
           expect(signature).not.toBe('');
-          expect(signature).toMatch(/^0x.*$/);
+          expect(signature).toMatch(/^0x[a-fA-F0-9]+$/);
         });
       });
 
@@ -152,33 +223,72 @@ describe('Test Turnkey EIP-1193 Provider', () => {
           const blockNumber = await eip1193Provider?.request({
             method: 'eth_blockNumber',
           });
-          expect(blockNumber).toBeDefined();
+          expect(blockNumber).not.toBeUndefined();
           expect(blockNumber).not.toBe('');
           expect(blockNumber).toMatch(/^0x.*$/);
+        });
+      });
+
+      describe('web3_clientVersion', () => {
+        it('should return the version of the client matching package.json version', async () => {
+          const version = await eip1193Provider?.request({
+            method: 'web3_clientVersion',
+          });
+          const packageJson = require('../../package.json');
+          const expectedVersion = `TurnkeyEIP1193Provider/v${packageJson.version}`;
+          expect(version).toBeDefined();
+          expect(version).toBe(expectedVersion);
         });
       });
     });
 
     describe('Public RPC Methods', () => {
       describe('eth_chainId', () => {
-        it(
-          'should get the correct chain id using the underlying RPC provider',
-          async () => {
-            const chainId = await eip1193Provider?.request({
-              method: 'eth_chainId',
-            });
-            expect(chainId).toBeDefined();
-            expect(chainId).not.toBe('');
-            expect(chainId).toMatch(/^0x.*$/);
-            expect(parseInt(chainId!, 16)).toBe(sepolia.id);
-          },
-          TEST_TIMEOUT
-        );
+        it('should get the correct chain id using the underlying RPC provider', async () => {
+          const chainId = await eip1193Provider?.request({
+            method: 'eth_chainId',
+          });
+          expect(chainId).not.toBeUndefined();
+          expect(chainId).toMatch(/^0x.*$/);
+          expect(parseInt(chainId!, 16)).toBe(sepolia.id);
+        });
       });
     });
   });
 
-  describe('Unsupported Methods', () => {});
+  describe('Unsupported Methods', () => {
+    describe('Unsupported Methods', () => {
+      const unsupportedMethods = [
+        'wallet_getPermissions',
+        'wallet_requestPermissions',
+        'wallet_revokePermissions',
+        'wallet_registerOnboarding',
+        'wallet_watchAsset',
+        'wallet_scanQRCode',
+        'wallet_getSnaps',
+        'wallet_requestSnaps',
+        'wallet_snap',
+        'wallet_invokeSnap',
+        'eth_decrypt',
+        'eth_getEncryptionPublicKey',
+      ];
+
+      unsupportedMethods.forEach((method) => {
+        it(`should throw a Method not supported error for ${method}`, async () => {
+          await expect(
+            eip1193Provider?.request({
+              method: method as EIP1474Methods[0]['Method'],
+            })
+          ).rejects.toEqual(
+            expect.objectContaining({
+              code: 4200,
+              message: expect.stringContaining('Method not supported'),
+            })
+          );
+        });
+      });
+    });
+  });
 });
 
 // describe(
--- packages/eip-1193-provider/src/turnkey.ts
+++ packages/eip-1193-provider/src/turnkey.ts
@@ -2,6 +2,11 @@ import {
   ApiKeyStamper,
   type TApiKeyStamperConfig,
 } from '@turnkey/api-key-stamper';
+import type { TurnkeyClient } from '@turnkey/http';
+import type { TSignRawPayloadResponse } from '@turnkey/http/src/__generated__/services/coordinator/public/v1/public_api.fetcher';
+import type { definitions } from '@turnkey/http/src/__generated__/services/coordinator/public/v1/public_api.types';
+import { signatureToHex } from 'viem';
+import { pad } from 'viem/utils';
 
 export const createAPIKeyStamper = (options?: TApiKeyStamperConfig) => {
   const apiPublicKey =
@@ -18,3 +23,68 @@ export const createAPIKeyStamper = (options?: TApiKeyStamperConfig) => {
     apiPrivateKey,
   });
 };
+
+export function unwrapActivityResult<
+  T extends definitions['v1ActivityResponse']
+>(
+  activityResponse: T,
+  { errorMessage }: { errorMessage: string }
+): T['activity']['result'] {
+  const { activity } = activityResponse;
+
+  switch (activity.status) {
+    case 'ACTIVITY_STATUS_CONSENSUS_NEEDED': {
+      throw 'Consensus needed';
+    }
+    case 'ACTIVITY_STATUS_COMPLETED': {
+      const result = activity.result;
+      if (result === undefined) {
+        throw 'Activity result is undefined';
+      }
+      return result;
+    }
+    default: {
+      throw errorMessage;
+    }
+  }
+}
+
+export async function signMessage({
+  client,
+  message,
+  organizationId,
+  signWith,
+}: {
+  client: TurnkeyClient;
+  message: `0x${string}`;
+  organizationId: string;
+  signWith: string;
+}): Promise<string> {
+  const activityResponse = await client.signRawPayload({
+    type: 'ACTIVITY_TYPE_SIGN_RAW_PAYLOAD_V2',
+    organizationId,
+    parameters: {
+      signWith,
+      payload: pad(message),
+      encoding: 'PAYLOAD_ENCODING_HEXADECIMAL',
+      hashFunction: 'HASH_FUNCTION_NO_OP',
+    },
+    timestampMs: String(Date.now()), // millisecond timestamp
+  });
+
+  const { signRawPayloadResult: signature } =
+    unwrapActivityResult<TSignRawPayloadResponse>(activityResponse, {
+      errorMessage: 'Error signing message',
+    });
+
+  if (!signature) {
+    // @todo update error message
+    throw 'Error signing message';
+  }
+
+  return signatureToHex({
+    r: `0x${signature.r}`,
+    s: `0x${signature.s}`,
+    v: BigInt(signature.v) + 27n,
+  });
+}
