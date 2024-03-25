544e8058ea9c587127780aa7b3e46c7ea7ab20ac
Andrew Min <andrew@turnkey.io>
2024-03-25 17:44:23 -0400

    implementation + example

--- packages/solana/src/__tests__/index-test.ts
+++ packages/solana/src/__tests__/index-test.ts
@@ -3,11 +3,13 @@ import { TurnkeySigner } from "../";
 import { TurnkeyClient } from "@turnkey/http";
 import { ApiKeyStamper } from "@turnkey/api-key-stamper";
 import {
+  SystemInstruction,
   PublicKey,
   SIGNATURE_LENGTH_IN_BYTES,
   SystemProgram,
   Transaction,
   TransactionMessage,
+  Version,
   VersionedTransaction,
 } from "@solana/web3.js";
 import nacl from "tweetnacl";
@@ -19,6 +21,7 @@ describe("TurnkeySigner", () => {
     "025d374c674fc389c761462f3c59c0acabdcb3a17c599d9e62e5fe78fe984cfbeb";
   const turnkeySolAddress = "D8P541wwnertZTgDT14kYJPoFT2eHUFqjTgPxMK5qatM";
   const defaultSignature = new Uint8Array(SIGNATURE_LENGTH_IN_BYTES);
+
   test("can sign a Solana transfer against production", async () => {
     if (!process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY) {
       // This test requires an env var to be set
@@ -109,7 +112,7 @@ describe("TurnkeySigner", () => {
 
     const transaction = new VersionedTransaction(messageV0);
 
-    // version transactions are initalized with a default signature
+    // version transactions are initialized with a default signature
     expect(transaction.signatures.length).toBe(1);
     expect(transaction.signatures[0]).toEqual(defaultSignature);
 
@@ -125,6 +128,159 @@ describe("TurnkeySigner", () => {
       bs58.decode(turnkeySolAddress)
     );
     expect(isValidSignature).toBeTruthy();
+  });
+
+  test("can sign multiple Solana transfers against production", async () => {
+    if (!process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY) {
+      // This test requires an env var to be set
+      throw new Error(
+        "This test requires SOLANA_TEST_ORG_API_PRIVATE_KEY to be set"
+      );
+    }
+
+    const client = new TurnkeyClient(
+      { baseUrl: "https://api.turnkey.com" },
+      new ApiKeyStamper({
+        apiPublicKey,
+        apiPrivateKey: process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY,
+      })
+    );
+
+    const signer = new TurnkeySigner({
+      organizationId,
+      client,
+    });
+
+    const transferTransaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: new PublicKey(turnkeySolAddress),
+        // Destination doesn't matter, we set it to the Turnkey war chest!
+        toPubkey: new PublicKey("tkhqC9QX2gkqJtUFk2QKhBmQfFyyqZXSpr73VFRi35C"),
+        lamports: 10000,
+      })
+    );
+
+    const transferTransaction2 = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: new PublicKey(turnkeySolAddress),
+        // Destination doesn't matter, we set it to the Turnkey war chest!
+        toPubkey: new PublicKey("tkhqC9QX2gkqJtUFk2QKhBmQfFyyqZXSpr73VFRi35C"),
+        lamports: 10000,
+      })
+    );
+
+    // Doesn't really matter since we're not going to broadcast this transaction!
+    // But if we don't set this the call to "serializeMessage fails."
+    transferTransaction.recentBlockhash =
+      "GZSq3KvoFVhE22CQsaWSAxBoAvRiZSs7xVNa6yPecMUu";
+    transferTransaction.feePayer = new PublicKey(turnkeySolAddress);
+    expect(transferTransaction.signatures.length).toBe(0);
+
+    const numTxs = 5;
+    const transactions = new Array<Transaction>();
+    for (let i = 0; i < numTxs; i++) {
+      transactions.push(transferTransaction);
+    }
+
+    const signedTransactions = await signer.signAllTransactions(
+      transactions,
+      turnkeySolAddress
+    );
+    expect(signedTransactions.length).toBe(numTxs);
+
+    for (let t of signedTransactions) {
+      const tx = t as Transaction;
+      const isValidSignature = nacl.sign.detached.verify(
+        tx.serializeMessage(),
+        tx.signature as Uint8Array,
+        bs58.decode(turnkeySolAddress)
+      );
+      expect(isValidSignature).toBeTruthy();
+    }
+  });
+
+  test("can sign multiple versioned Solana transfers against production", async () => {
+    if (!process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY) {
+      // This test requires an env var to be set
+      throw new Error(
+        "This test requires SOLANA_TEST_ORG_API_PRIVATE_KEY to be set"
+      );
+    }
+
+    const client = new TurnkeyClient(
+      { baseUrl: "https://api.turnkey.com" },
+      new ApiKeyStamper({
+        apiPublicKey,
+        apiPrivateKey: process.env.SOLANA_TEST_ORG_API_PRIVATE_KEY,
+      })
+    );
+
+    const signer = new TurnkeySigner({
+      organizationId,
+      client,
+    });
+
+    const fromKey = new PublicKey(turnkeySolAddress);
+
+    const instructions = [
+      SystemProgram.transfer({
+        fromPubkey: fromKey,
+        // Destination doesn't matter, we set it to the Turnkey war chest!
+        toPubkey: new PublicKey("tkhqC9QX2gkqJtUFk2QKhBmQfFyyqZXSpr73VFRi35C"),
+        lamports: Math.floor(Math.random() * 100), // random amount
+      }),
+    ];
+
+    // create v0 compatible message
+    const messageV0 = new TransactionMessage({
+      payerKey: fromKey,
+      // Doesn't really matter since we're not going to broadcast this transaction!
+      recentBlockhash: "GZSq3KvoFVhE22CQsaWSAxBoAvRiZSs7xVNa6yPecMUu",
+      instructions,
+    }).compileToV0Message();
+
+    const transaction = new VersionedTransaction(messageV0);
+
+    // version transactions are initialized with a default signature
+    expect(transaction.signatures.length).toBe(1);
+    expect(transaction.signatures[0]).toEqual(defaultSignature);
+
+    const numTxs = 5;
+    const transactions = new Array<VersionedTransaction>();
+    for (let i = 0; i < numTxs; i++) {
+      transactions.push(transaction);
+    }
+
+    const signedTransactions = await signer.signAllTransactions(
+      transactions,
+      turnkeySolAddress
+    );
+    expect(signedTransactions.length).toBe(numTxs);
+
+    for (let t of signedTransactions) {
+      const tx = t as VersionedTransaction;
+      // after signing the version transaction, the default signature is replaced with the new one
+      expect(tx.signatures.length).toBe(1);
+      expect(tx.signatures[0]).not.toEqual(defaultSignature);
+
+      const isValidSignature = nacl.sign.detached.verify(
+        tx.message.serialize(),
+        tx.signatures[0] as Uint8Array,
+        bs58.decode(turnkeySolAddress)
+      );
+      expect(isValidSignature).toBeTruthy();
+
+      SystemInstruction.decodeTransfer(tx.message.compiledInstructions[0])
+
+      console.log(tx.message.compiledInstructions);
+      console.log(tx.serialize());
+      console.log(Buffer.from(tx.signatures[0]).toString("hex"));
+
+      // ensure ordering is preserved
+      // console.log('individual signed tx', tx.message.compiledInstructions);
+      // Transaction.parse
+      // const decoded = SystemInstruction.decodeTransfer(tx.message.compiledInstructions[0]);
+    }
   });
 
   test("can sign a message with a Solana account", async () => {
