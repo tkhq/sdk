5b12bb94e4210a272b4ed02e0b0fa8598b16f29c
Taylor Dawson <taylor@turnkey.io>
2024-03-27 20:39:59 +0000

    fix rollup config

--- packages/eip-1193-provider/rollup.config.mjs
+++ packages/eip-1193-provider/rollup.config.mjs
@@ -1,18 +1,3 @@
-import rollupBaseConfig from '../../rollup.config.base.mjs';
-import versionInjector from 'rollup-plugin-version-injector';
+import rollup from '../../rollup.config.base.mjs';
 
-export default () => {
-  const configs = rollupBaseConfig(); // This should be an array of configurations
-
-  // Add the versionInjector plugin to each configuration
-  configs.forEach((config) => {
-    config.plugins = [
-      ...(config.plugins || []),
-      versionInjector({
-        logLevel: 'warn',
-      }),
-    ];
-  });
-
-  return configs;
-};
+export default (options) => rollup();
