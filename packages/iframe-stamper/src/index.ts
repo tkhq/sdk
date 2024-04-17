1c9a72fe2d398c5b28ddbf74e3149ffb821b6308
Arnaud Brousseau <rno@turnkey.io>
2024-04-17 12:26:12 -0500

    Add sandbox attribute

--- packages/iframe-stamper/src/index.ts
+++ packages/iframe-stamper/src/index.ts
@@ -131,6 +131,11 @@ export class IframeStamper {
     }
 
     let iframe = window.document.createElement("iframe");
+
+    // See https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#sandbox
+    // We do not need any other permission than running scripts for import/export/auth frames.
+    iframe.setAttribute("sandbox", "allow-scripts");
+
     iframe.id = config.iframeElementId;
     iframe.src = config.iframeUrl;
 
