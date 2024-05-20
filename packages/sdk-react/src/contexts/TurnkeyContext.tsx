4a08fb73da63dbbaf7e5c77679aecdfeaeff412e
Andrew Min <andrew@turnkey.io>
2024-05-20 11:43:14 -0400

    getActiveClient

--- packages/sdk-react/src/contexts/TurnkeyContext.tsx
+++ packages/sdk-react/src/contexts/TurnkeyContext.tsx
@@ -4,18 +4,23 @@ import {
   TurnkeyIframeClient,
   TurnkeyPasskeyClient,
   TurnkeySDKBrowserConfig,
+  TurnkeyBrowserClient,
 } from "@turnkey/sdk-browser";
 
 export interface TurnkeyClientType {
   turnkey: Turnkey | undefined;
   authIframeClient: TurnkeyIframeClient | undefined;
   passkeyClient: TurnkeyPasskeyClient | undefined;
+  getActiveClient: () => Promise<TurnkeyBrowserClient | undefined>;
 }
 
 export const TurnkeyContext = createContext<TurnkeyClientType>({
   turnkey: undefined,
   passkeyClient: undefined,
   authIframeClient: undefined,
+  getActiveClient: async () => {
+    return undefined;
+  },
 });
 
 interface TurnkeyProviderProps {
@@ -39,13 +44,44 @@ export const TurnkeyProvider: React.FC<TurnkeyProviderProps> = ({
   const TurnkeyAuthIframeContainerId = "turnkey-auth-iframe-container-id";
   const TurnkeyAuthIframeElementId = "turnkey-auth-iframe-element-id";
 
+  const getActiveClient = async () => {
+    let currentClient: TurnkeyBrowserClient | undefined = passkeyClient;
+
+    try {
+      const authBundle = await turnkey?.getAuthBundle();
+
+      if (authBundle) {
+        const injected = await authIframeClient?.injectCredentialBundle(
+          authBundle
+        );
+        if (injected) {
+          const currentUser = await turnkey?.getCurrentUser();
+          await authIframeClient?.getWhoami({
+            organizationId:
+              currentUser?.organization.organizationId ??
+              turnkey?.config.defaultOrganizationId!,
+          });
+
+          currentClient = authIframeClient;
+        }
+      }
+    } catch (err: any) {
+      console.error("Failed to use iframe client", err);
+      console.log("Defaulting to passkey client");
+    }
+
+    return currentClient;
+  };
+
   useEffect(() => {
     (async () => {
       if (!iframeInit.current) {
         iframeInit.current = true;
+
         const newTurnkey = new Turnkey(config);
         setTurnkey(newTurnkey);
         setPasskeyClient(newTurnkey.passkeyClient());
+
         const newAuthIframeClient = await newTurnkey.iframeClient({
           iframeContainer: document.getElementById(
             TurnkeyAuthIframeContainerId
@@ -64,6 +100,7 @@ export const TurnkeyProvider: React.FC<TurnkeyProviderProps> = ({
         turnkey,
         passkeyClient,
         authIframeClient,
+        getActiveClient,
       }}
     >
       {children}
