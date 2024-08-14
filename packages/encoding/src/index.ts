883dc2627023422cd3b09ff7bcfa01539dbd2480
Andrew Min <andrew@turnkey.io>
2024-08-14 11:42:27 -0700

    feedback: remove currently unused utils from packages/encoding

--- packages/encoding/src/index.ts
+++ packages/encoding/src/index.ts
@@ -9,44 +9,6 @@ export function stringToBase64urlString(input: string): string {
   return base64StringToBase64UrlEncodedString(base64String);
 }
 
-export function base64urlToBuffer(baseurl64String: string): ArrayBuffer {
-  // Base64url to Base64
-  const padding = "==".slice(0, (4 - (baseurl64String.length % 4)) % 4);
-  const base64String =
-    baseurl64String.replace(/-/g, "+").replace(/_/g, "/") + padding;
-
-  // Base64 to binary string
-  const str = atob(base64String);
-
-  // Binary string to buffer
-  const buffer = new ArrayBuffer(str.length);
-  const byteView = new Uint8Array(buffer);
-  for (let i = 0; i < str.length; i++) {
-    byteView[i] = str.charCodeAt(i);
-  }
-  return byteView;
-}
-
-export function bufferToBase64url(buffer: ArrayBuffer): string {
-  // Buffer to binary string
-  const byteView = new Uint8Array(buffer);
-  let str = "";
-  for (const charCode of byteView) {
-    str += String.fromCharCode(charCode);
-  }
-
-  // Binary string to base64
-  const base64String = btoa(str);
-
-  // Base64 to base64url
-  // We assume that the base64url string is well-formed.
-  const base64urlString = base64String
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=/g, "");
-  return base64urlString;
-}
-
 export function hexStringToBase64url(input: string, length?: number): string {
   // Add an extra 0 to the start of the string to get a valid hex string (even length)
   // (e.g. 0x0123 instead of 0x123)
