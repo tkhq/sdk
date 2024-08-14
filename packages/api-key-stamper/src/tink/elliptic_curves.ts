8c0f6ab2a1a8e89afaa525ea54159fc5c04953ec
Andrew Min <andrew@turnkey.io>
2024-08-14 13:44:19 -0700

    remove optionally comment

--- packages/api-key-stamper/src/tink/elliptic_curves.ts
+++ packages/api-key-stamper/src/tink/elliptic_curves.ts
@@ -1,6 +1,6 @@
 /**
  * Code modified from https://github.com/google/tink/blob/6f74b99a2bfe6677e3670799116a57268fd067fa/javascript/subtle/elliptic_curves.ts
- * - The implementation of integerToByteArray has been modified to optionally augment the resulting byte array to a certain length.
+ * - The implementation of integerToByteArray has been modified to augment the resulting byte array to a certain length.
  *
  * @license
  * Copyright 2020 Google LLC
