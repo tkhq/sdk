a8e295b651685ec642714dc1e83e308595379a20
Taylor Dawson <taylor@turnkey.io>
2024-08-20 12:01:00 -0700

    Run prettier

--- packages/eip-1193-provider/src/constants.ts
+++ packages/eip-1193-provider/src/constants.ts
@@ -4,19 +4,19 @@ export const PROVIDER_ERROR_MESSAGE = {
   INVALID_CHAIN_ID_HEX:
     "Expected 0x-prefixed, unpadded, non-zero hexadecimal string 'chainId'. Received: ",
   INVALID_CHAIN_ID_VALUE:
-    'Expected numerical value greater than max safe value. Received: ',
+    "Expected numerical value greater than max safe value. Received: ",
   NATIVE_CURRENCY_SYMBOL_LENGTH:
     "Expected 2-6 character string 'nativeCurrency.symbol'. Received: ",
   NATIVE_CURRENCY_SYMBOL_MISMATCH:
-    'nativeCurrency.symbol does not match currency symbol for a network the user already has added with the same chainId. Received: ',
+    "nativeCurrency.symbol does not match currency symbol for a network the user already has added with the same chainId. Received: ",
   INVALID_RPC_URL:
-    'rpcUrls field is required and must contain at least one valid HTTP/HTTPS URL',
+    "rpcUrls field is required and must contain at least one valid HTTP/HTTPS URL",
   UNRECOGNIZED_CHAIN_ID:
-    'Unrecognized chain ID. Try adding the chain using wallet_addEthereumChain first. Received: ',
+    "Unrecognized chain ID. Try adding the chain using wallet_addEthereumChain first. Received: ",
   CHAIN_ID_RPC_MISMATCH:
-    'Chain ID does not match the RPC endpoint. Provider Chain ID: ',
-  PROVIDER_DISCONNECTED: 'Provider is disconnected from chain.',
-  RPC_URLS_REQUIRED: 'rpcUrls field is required and cannot be empty',
+    "Chain ID does not match the RPC endpoint. Provider Chain ID: ",
+  PROVIDER_DISCONNECTED: "Provider is disconnected from chain.",
+  RPC_URLS_REQUIRED: "rpcUrls field is required and cannot be empty",
 };
 
 export const PROVIDER_ERROR_CODE = {
