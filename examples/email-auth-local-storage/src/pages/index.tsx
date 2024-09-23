e25359ff32dd10b5c629c2728ca4c5697426dd19
Mohammad Cheikh <moe@turnkey.io>
2024-09-23 15:38:28 -0400

    fix email auth local storage example

--- examples/email-auth-local-storage/src/pages/index.tsx
+++ examples/email-auth-local-storage/src/pages/index.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { Turnkey } from "@turnkey/sdk-server";
 import {
   generateP256KeyPair,
-  decryptBundle,
+  decryptEmailBundle,
   getPublicKey,
 } from "@turnkey/crypto";
 import {
@@ -121,13 +121,7 @@ export default function AuthPage() {
       const parsed = JSON.parse(embeddedKey);
 
       // This is decrypting the user-provided email auth bundle using the locally stored target embedded key
-      const decryptedDataRaw = decryptBundle(
-        data.authBundle,
-        parsed.privateKey
-      ) as Uint8Array;
-
-      // This is the resulting decrypted Turnkey API key
-      decryptedData = uint8ArrayToHexString(decryptedDataRaw);
+      decryptedData = decryptEmailBundle(data.authBundle, parsed.privateKey);
 
       // Save the email auth bundle to local storage as well. This can be reused in order for the
       // end user to avoid having to email auth repeatedly
@@ -200,13 +194,10 @@ export default function AuthPage() {
       );
 
       // This is decrypting the locally stored email auth bundle using the locally stored target embedded key
-      const decryptedDataRaw = decryptBundle(
+      decryptedData = decryptEmailBundle(
         localCredentialBundle,
         parsedKey.privateKey
-      ) as Uint8Array;
-
-      // This is the resulting decrypted Turnkey API key
-      decryptedData = uint8ArrayToHexString(decryptedDataRaw);
+      );
     } catch (e) {
       const msg = `Error while injecting bundle: ${e}`;
       console.error(msg);
