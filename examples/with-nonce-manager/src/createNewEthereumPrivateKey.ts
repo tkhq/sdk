8d15b22fc1bdf5b9c3116a3f0f6d0d43e05a00d9
Andrew Min <andrew@turnkey.io>
2024-10-16 11:27:00 -0400

    update nonce manager example

--- examples/with-nonce-manager/src/createNewEthereumPrivateKey.ts
+++ examples/with-nonce-manager/src/createNewEthereumPrivateKey.ts
@@ -1,46 +1,32 @@
-import { TurnkeyClient } from "@turnkey/http";
-import { createActivityPoller } from "@turnkey/http";
-import { ApiKeyStamper } from "@turnkey/api-key-stamper";
-import { TurnkeyActivityError } from "@turnkey/ethers";
+import {
+  Turnkey as TurnkeySDKServer,
+  TurnkeyActivityError,
+} from "@turnkey/sdk-server";
 import * as crypto from "crypto";
 import { refineNonNull } from "./util";
 
 export async function createNewEthereumPrivateKey() {
-  const turnkeyClient = new TurnkeyClient(
-    { baseUrl: process.env.BASE_URL! },
-    new ApiKeyStamper({
-      apiPublicKey: process.env.API_PUBLIC_KEY!,
-      apiPrivateKey: process.env.API_PRIVATE_KEY!,
-    })
-  );
-
-  const activityPoller = createActivityPoller({
-    client: turnkeyClient,
-    requestFn: turnkeyClient.createPrivateKeys,
+  const turnkeyClient = new TurnkeySDKServer({
+    apiBaseUrl: "https://api.turnkey.com",
+    apiPublicKey: process.env.API_PUBLIC_KEY!,
+    apiPrivateKey: process.env.API_PRIVATE_KEY!,
+    defaultOrganizationId: process.env.ORGANIZATION_ID!,
   });
 
   const privateKeyName = `ETH Key ${crypto.randomBytes(2).toString("hex")}`;
 
   try {
-    const activity = await activityPoller({
-      type: "ACTIVITY_TYPE_CREATE_PRIVATE_KEYS_V2",
-      organizationId: process.env.ORGANIZATION_ID!,
-      parameters: {
-        privateKeys: [
-          {
-            privateKeyName,
-            curve: "CURVE_SECP256K1",
-            addressFormats: ["ADDRESS_FORMAT_ETHEREUM"],
-            privateKeyTags: [],
-          },
-        ],
-      },
-      timestampMs: String(Date.now()), // millisecond timestamp
+    const { privateKeys } = await turnkeyClient.apiClient().createPrivateKeys({
+      privateKeys: [
+        {
+          privateKeyName,
+          curve: "CURVE_SECP256K1",
+          addressFormats: ["ADDRESS_FORMAT_ETHEREUM"],
+          privateKeyTags: [],
+        },
+      ],
     });
 
-    const privateKeys = refineNonNull(
-      activity.result.createPrivateKeysResultV2?.privateKeys
-    );
     const privateKeyId = refineNonNull(privateKeys?.[0]?.privateKeyId);
     const address = refineNonNull(privateKeys?.[0]?.addresses?.[0]?.address);
 
