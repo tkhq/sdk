7fb78bfc881bded702032a4f3995ce33fad2bc9f
Mohammad Cheikh <moe@turnkey.io>
2024-10-31 11:56:10 -0400

    reset oauth

--- examples/oauth/src/pages/_app.tsx
+++ examples/oauth/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import '@turnkey/sdk-react/styles';
+
 import { TurnkeyProvider } from "@turnkey/sdk-react";
 
 const turnkeyConfig = {
