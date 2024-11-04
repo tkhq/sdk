d6bf3494c5148fff6d45821de67e3916a9f2671f
Andrew Min <andrew@turnkey.io>
2024-11-03 23:48:34 -0500

    styling nits

--- examples/wallet-import-export/src/components/Export.tsx
+++ examples/wallet-import-export/src/components/Export.tsx
@@ -10,6 +10,36 @@ interface ExportProps {
   setIframeStamper: Dispatch<SetStateAction<IframeStamper | null>>;
 }
 
+const styles = {
+  padding: "20px",
+  borderRadius: "8px",
+  borderWidth: "2px",
+  borderStyle: "solid",
+  borderColor: "#ff6961",
+  fontFamily: "monospace",
+  color: "#333",
+  width: "340px",
+  height: "100px",
+  backgroundColor: "#ffd966",
+  boxShadow: "0px 0px 10px #aaa",
+  overflowWrap: "break-word",
+  wordWrap: "break-word",
+  resize: "none",
+};
+
+const iframeCss = `
+  iframe {
+    box-sizing: border-box;
+    width: 400px;
+    height: 120px;
+    border-radius: 8px;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgba(216, 219, 227, 1);
+    padding: 20px;
+  }
+`;
+
 const TurnkeyIframeContainerId = "turnkey-export-iframe-container-id";
 const TurnkeyIframeElementId = "turnkey-export-iframe-element-id";
 
@@ -43,22 +73,6 @@ export function Export(props: ExportProps) {
           return iframeStamper;
         })
         .then((iframeStamper: IframeStamper) => {
-          const styles = {
-            padding: "20px",
-            borderRadius: "8px",
-            borderWidth: "2px",
-            borderStyle: "solid",
-            borderColor: "#ff6961",
-            fontFamily: "monospace",
-            color: "#333",
-            width: "340px",
-            height: "100px",
-            backgroundColor: "#ffd966",
-            boxShadow: "0px 0px 10px #aaa",
-            overflowWrap: "break-word",
-            wordWrap: "break-word",
-            resize: "none",
-          };
           return iframeStamper.applySettings({ styles });
         })
         .then((settingsApplied: boolean) => {
@@ -75,19 +89,6 @@ export function Export(props: ExportProps) {
       }
     };
   }, [props.setIframeStamper, iframeStamper, setIframeStamper]);
-
-  const iframeCss = `
-    iframe {
-      box-sizing: border-box;
-      width: 400px;
-      height: 120px;
-      border-radius: 8px;
-      border-width: 1px;
-      border-style: solid;
-      border-color: rgba(216, 219, 227, 1);
-      padding: 20px;
-    }
-    `;
 
   return (
     <div style={{ display: iframeDisplay }} id={TurnkeyIframeContainerId}>
