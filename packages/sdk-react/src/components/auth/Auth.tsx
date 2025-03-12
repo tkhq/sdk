98b7a9d45995acb3b58fa5c17aae2b5ba2d99424
Brian Cleary <brian.cleary@turnkey.io>
2025-03-12 12:29:42 -0400

    feat: add walletClient

--- packages/sdk-react/src/components/auth/Auth.tsx
+++ packages/sdk-react/src/components/auth/Auth.tsx
@@ -88,7 +88,7 @@ const Auth: React.FC<AuthProps> = ({
   customSmsMessage,
   customAccounts,
 }) => {
-  const { authIframeClient, passkeyClient } = useTurnkey();
+  const { authIframeClient, passkeyClient, walletClient } = useTurnkey();
   const router = useRouter();
   const [email, setEmail] = useState<string>("");
   const [phone, setPhone] = useState<string>("");
@@ -139,13 +139,13 @@ const Auth: React.FC<AuthProps> = ({
 
   const handleAuthSuccess = async (
     credentialBundle: any,
-    expirationSeconds?: string,
+    expirationSeconds?: string
   ) => {
     if (credentialBundle) {
       await authIframeClient!.injectCredentialBundle(credentialBundle);
       await authIframeClient!.loginWithAuthBundle(
         credentialBundle,
-        expirationSeconds,
+        expirationSeconds
       );
       await onAuthSuccess();
     }
@@ -195,7 +195,7 @@ const Auth: React.FC<AuthProps> = ({
       await passkeyClient?.loginWithPasskey(
         SessionType.READ_WRITE,
         authIframeClient!,
-        authIframeClient?.iframePublicKey!,
+        authIframeClient?.iframePublicKey!
       );
       router.push("/dashboard");
     } catch {
@@ -208,7 +208,7 @@ const Auth: React.FC<AuthProps> = ({
       await passkeyClient?.loginWithPasskey(
         SessionType.READ_WRITE,
         authIframeClient!,
-        authIframeClient?.iframePublicKey!,
+        authIframeClient?.iframePublicKey!
       );
       router.push("/dashboard");
     } catch (error) {
@@ -219,7 +219,7 @@ const Auth: React.FC<AuthProps> = ({
   const handleOtpLogin = async (
     type: FilterType.Email | FilterType.PhoneNumber,
     value: string,
-    otpType: string,
+    otpType: string
   ) => {
     const createSuborgData: Record<string, any> = {};
     if (type === FilterType.Email) createSuborgData.email = value;
