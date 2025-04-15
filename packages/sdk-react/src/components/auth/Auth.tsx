0eb90df9cd8c1479b4b936ef7e864cba7d42848a
Brian Cleary <brian.cleary@turnkey.io>
2025-04-15 15:40:31 -0400

    feat: update changeset schema and config

--- packages/sdk-react/src/components/auth/Auth.tsx
+++ packages/sdk-react/src/components/auth/Auth.tsx
@@ -18,11 +18,8 @@ import { FilterType, OtpType, authErrors } from "./constants";
 import type { TurnkeyApiTypes, WalletAccount } from "@turnkey/sdk-browser";
 import { server } from "@turnkey/sdk-server";
 import parsePhoneNumberFromString from "libphonenumber-js";
+import { SessionType } from "@turnkey/sdk-types";
 
-export enum SessionType {
-  READ_ONLY = "SESSION_TYPE_READ_ONLY",
-  READ_WRITE = "SESSION_TYPE_READ_WRITE",
-}
 export interface PasskeyConfig {
   displayName?: string;
   name?: string;
@@ -244,7 +241,7 @@ const Auth: React.FC<AuthProps> = ({
   const handleOtpLogin = async (
     type: FilterType.Email | FilterType.PhoneNumber,
     value: string,
-    otpType: string,
+    otpType: string
   ) => {
     setLoading(otpType);
     const createSuborgData: Record<string, any> = {};
