77ea79c0b7d91b44b70eeeb6c651124cb8158d66
Mohammed Odeh <moe.odeh@turnkey.io>
2025-04-16 17:27:42 -0400

    update origin url

--- packages/sdk-react-native/src/constants.ts
+++ packages/sdk-react-native/src/constants.ts
@@ -10,5 +10,5 @@ export const OTP_AUTH_DEFAULT_EXPIRATION_SECONDS = 15 * 60;
 export const MAX_SESSIONS = 15;
 export const SESSION_WARNING_THRESHOLD_SECONDS = 15;
 
-export const TURNKEY_OAUTH_ORIGIN_URL = "https://oauth-redirect.preprod.turnkey.engineering"; // TODO: we need to change this when we go deploy to prod
+export const TURNKEY_OAUTH_ORIGIN_URL = "https://oauth-origin.preprod.turnkey.engineering"; // TODO: we need to change this when we go deploy to prod
 export const TURNKEY_REDIRECT_ORIGIN_URL = "https://oauth-redirect.preprod.turnkey.engineering"; // TODO: we need to change this when we go deploy to prod
