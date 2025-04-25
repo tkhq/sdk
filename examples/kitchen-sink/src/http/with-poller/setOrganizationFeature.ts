862e132ad0b4d162eab0dd0844758c4880426f12
Andrew Min <andrew@turnkey.io>
2025-04-24 22:19:26 -0400

    add with-poller folder

---
+++ examples/kitchen-sink/src/http/with-poller/setOrganizationFeature.ts
@@ -0,0 +1,39 @@
+import * as path from "path";
+import * as dotenv from "dotenv";
+
+// Load environment variables from `.env.local`
+dotenv.config({ path: path.resolve(process.cwd(), ".env.local") });
+
+import { TurnkeyClient } from "@turnkey/http";
+import { ApiKeyStamper } from "@turnkey/api-key-stamper";
+
+async function main() {
+  // Initialize a Turnkey client
+  const turnkeyClient = new TurnkeyClient(
+    { baseUrl: process.env.BASE_URL! },
+    new ApiKeyStamper({
+      apiPublicKey: process.env.API_PUBLIC_KEY!,
+      apiPrivateKey: process.env.API_PRIVATE_KEY!,
+    }),
+  );
+
+  const { activity } = await turnkeyClient.setOrganizationFeature({
+    type: "ACTIVITY_TYPE_SET_ORGANIZATION_FEATURE",
+    timestampMs: String(Date.now()),
+    organizationId: process.env.ORGANIZATION_ID!,
+    parameters: {
+      name: "FEATURE_NAME_EMAIL_AUTH",
+      value: "",
+    },
+  });
+
+  console.log(
+    "Successfully set organization feature. Updated features:",
+    activity.result.setOrganizationFeatureResult?.features,
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
