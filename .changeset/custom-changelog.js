bf4127a8263b19d2178fba2beb49c7d8d1db36ae
Brian Cleary <brian.cleary@turnkey.io>
2025-05-13 13:58:04 -0400

    feat: update changelog format

--- .changeset/custom-changelog.js
+++ .changeset/custom-changelog.js
@@ -6,23 +6,20 @@ module.exports = {
     const releaseLine = await githubChangelog.getReleaseLine(
       changeset,
       type,
-      options,
+      options
     );
 
     // replace "Thanks [username]!" with "Author [username]"
-    console.log("Original release line:", releaseLine);
     const regex = /Thanks (\[@\w+\]\(https:\/\/github\.com\/\w+\))!/;
-    const updatedReleaseLine = releaseLine.replace(regex, "Author $1");
 
-    console.log("Updated release line:", updatedReleaseLine);
-    return updatedReleaseLine;
+    return releaseLine.replace(regex, "Author $1");
   },
   async getDependencyReleaseLine(changesets, dependenciesUpdated, options) {
     // reuse the original dependency release line logic
     return githubChangelog.getDependencyReleaseLine(
       changesets,
       dependenciesUpdated,
-      options,
+      options
     );
   },
 };
