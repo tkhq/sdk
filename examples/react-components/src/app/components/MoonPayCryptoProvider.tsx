375e76799a82624aacf1253295c3c977aad92115
Brian Cleary <brian.cleary@turnkey.io>
2025-05-30 11:29:04 -0400

    chore: coinbase and moonpay integration

---
+++ examples/react-components/src/app/components/MoonPayCryptoProvider.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import dynamic from "next/dynamic";
+
+const MoonPayProvider = dynamic(
+  () => import("@moonpay/moonpay-react").then((mod) => mod.MoonPayProvider),
+  { ssr: false },
+);
+
+interface MoonPayCryptoProviderProps {
+  children: React.ReactNode;
+}
+
+const MoonPayCryptoProvider = ({ children }: MoonPayCryptoProviderProps) => {
+  return (
+    <MoonPayProvider apiKey={process.env.NEXT_PUBLIC_MOONPAY_API_KEY!}>
+      {children}
+    </MoonPayProvider>
+  );
+};
+
+export default MoonPayCryptoProvider;
