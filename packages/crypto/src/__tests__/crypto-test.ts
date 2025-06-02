fff539912453e3f9c390d07e4cf12d4bb16ad2c6
Mohammad Cheikh <moe@turnkey.io>
2025-06-02 11:33:12 -0400

    add function to verify session jwt

--- packages/crypto/src/__tests__/crypto-test.ts
+++ packages/crypto/src/__tests__/crypto-test.ts
@@ -15,6 +15,7 @@ import {
   hpkeAuthEncrypt,
   formatHpkeBuf,
   verifyStampSignature,
+  verifySessionJwtSignature,
 } from "../";
 
 // Mock data for testing
@@ -225,5 +226,17 @@ describe("Turnkey Crypto Primitives", () => {
     );
 
     expect(verified).toEqual(true);
+  });
+});
+
+describe("Session JWT signature", () => {
+  test("verifies the provided JWT against its public key", async () => {
+    const jwt =
+      "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9." +
+      "eyJleHAiOjE3NDg4NzY4MzcsInB1YmxpY19rZXkiOiIwMzk5ZmUyYWNlNjIwOGFmMGFkZjg0OGY0NGJjNDgyMTBiNTk0YjdlNjllY2Q5MWVjOTY4ZmQ3NWIzYmI0NDgzMzYiLCJzZXNzaW9uX3R5cGUiOiJTRVNTSU9OX1RZUEVfUkVBRF9XUklURSIsInVzZXJfaWQiOiI2OTEyYjgxOS1mNGRmLTQwZjQtYTE5Mi0yMGVlNDMwOTA5NzQiLCJvcmdhbml6YXRpb25faWQiOiJjNzVlY2IwNy1jODRhLTRkZDUtOTMyYy01MzlkZmFmYzY4NjQifQ." +
+      "y6LPW1jlTwc9jFcvCwKJoKfleL_vHnGUr5tRVdMFUCnHvDspSPZ3DWK85tf1znCCBFQ6MYaFOl-1FLb0KcFxqQ";
+
+    const ok = await verifySessionJwtSignature(jwt);
+    expect(ok).toBe(true);
   });
 });
