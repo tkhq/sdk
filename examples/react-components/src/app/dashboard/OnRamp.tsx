f67a1553181eb88ce8166eae2f9c640f6cdde3ed
Brian Cleary <brian.cleary@turnkey.io>
2025-06-06 13:00:06 -0400

    chore: integrate with OnchainKit hooks and MoonPay widget

--- examples/react-components/src/app/dashboard/OnRamp.tsx
+++ examples/react-components/src/app/dashboard/OnRamp.tsx
@@ -1,8 +1,15 @@
 import crypto from "crypto";
 import { MoonPayBuyWidget } from "@moonpay/moonpay-react";
-import { FundButton } from "@coinbase/onchainkit/fund";
+import {
+  FundButton,
+  getOnrampBuyUrl,
+  fetchOnrampConfig,
+  fetchOnrampOptions,
+} from "@coinbase/onchainkit/fund";
 import { useEffect, useState } from "react";
 import { useTurnkey } from "@turnkey/sdk-react";
+import { Buy } from "@coinbase/onchainkit/buy";
+import { Token } from "@coinbase/onchainkit/token";
 
 export const OnRamp = () => {
   const { turnkey, indexedDbClient } = useTurnkey();
@@ -12,6 +19,14 @@ export const OnRamp = () => {
   const [isCoinbaseToggled, setIsCoinbaseToggled] = useState(false);
   const [isMoonPayToggled, setIsMoonPayToggled] = useState(false);
 
+  const ethToken: Token = {
+    name: "ETH",
+    address: "0x652bd17D489F283A03bb52DAFa138764Be04Bc66",
+    symbol: "ETH",
+    decimals: 6,
+    chainId: 8453,
+    image: "",
+  };
   // useEffect(() => {
   //   const loadMoonPaySdk = async () => {
   //     try {
@@ -44,17 +59,60 @@ export const OnRamp = () => {
           organizationId: session?.organizationId!,
           onrampProvider: "FIAT_ON_RAMP_PROVIDER_COINBASE",
           transactionType: "FIAT_ON_RAMP_TRANSACTION_TYPE_BUY",
+          walletAddress: "0x652bd17D489F283A03bb52DAFa138764Be04Bc66",
+          fiatCurrencyCode: "USD",
         });
 
+        const onrampConfig = await fetchOnrampConfig();
+        console.log("onrampConfig:", onrampConfig);
+
+        const onrampOptions = await fetchOnrampOptions({
+          country: "US",
+          subdivision: "ME",
+        });
+        console.log("onrampOptions:", onrampOptions);
+
+        const onrampBuyUrl = getOnrampBuyUrl({
+          projectId: "aefedf19-488c-426c-b7be-133fab72807c",
+          addresses: {
+            ["0x652bd17D489F283A03bb52DAFa138764Be04Bc66"]: ["base"],
+          },
+          assets: ["ETH"],
+          presetFiatAmount: 20,
+          fiatCurrency: "USD",
+        });
+
+        // <FundButton fundingUrl={onrampBuyUrl} />;
+
+        console.log("Coinbase onrampBuyUrl:", onrampBuyUrl);
+        setCoinbaseOnRampBuyUrl(onrampBuyUrl);
         console.log("initFiatOnRampResponse:", initFiatOnRampResponse);
-        if (initFiatOnRampResponse?.onRampUrl) {
-          setCoinbaseOnRampBuyUrl(initFiatOnRampResponse?.onRampUrl);
-        }
+        // if (initFiatOnRampResponse?.onRampUrl) {
+        //   setCoinbaseOnRampBuyUrl(initFiatOnRampResponse?.onRampUrl);
+        // }
       } catch (error) {
         console.error("Failed to init fiat on ramp:", error);
       }
     };
 
+    // https://pay.coinbase.com/buy?sessionToken=MWYwNDE2YTItYWMwZC02OTA0LThjMDAtMTY3NzNhNGRkNGRk&appId=aefedf19-488c-426c-b7be-133fab72807c&destinationWallets=%5B%7B%22address%22%3A%220x652bd17D489F283A03bb52DAFa138764Be04Bc66%22%2C%22blockchains%22%3A%5B%22ethereum%22%5D%7D%5D&defaultAsset=ETH&defaultPaymentMethod=CARD&fiatCurrency=USD&presetFiatAmount=10
+    // https://pay.coinbase.com/buy/select-payment-method?appId=58a3fa2e-617f-4198-81e7-096f5e498c00&defaultAsset=ETH&defaultPaymentMethod=CARD&destinationWallets=%5B%7B%22address%22%3A%220x652bd17D489F283A03bb52DAFa138764Be04Bc66%22%2C%22blockchains%22%3A%5B%22ethereum%22%5D%7D%5D&fiatCurrency=USD&presetFiatAmount=10
+    // https://pay.coinbase.com/buy?token=MWYwNDE2YmQtZjEyOS02NTk1LWIxYjMtODIxNTE0ZDI3MTM5&appId=aefedf19-488c-426c-b7be-133fab72807c&destinationWallets=%5B%7B%22address%22%3A%220x652bd17D489F283A03bb52DAFa138764Be04Bc66%22%2C%22blockchains%22%3A%5B%22ethereum%22%5D%7D%5D&defaultAsset=ETH&defaultPaymentMethod=CARD&fiatCurrency=USD&presetFiatAmount=10
+    // https://pay.coinbase.com/buy?sessionToken=MWYwNDE2Y2MtMDhlZC02MjdmLTgwNDgtMTY5MmQ0NGIwMTFh&defaultAsset=ETH&defaultPaymentMethod=CARD&fiatCurrency=USD&presetFiatAmount=10
+
+    // example from coinbase docs https://docs.cdp.coinbase.com/onramp/docs/api-oneclickbuy#generating-one-click-buy-urls
+    // https://pay.coinbase.com/buy/select-asset?appId=58a3fa2e-617f-4198-81e7-096f5e498c00&destinationWallets=[{"address":"0x652bd17D489F283A03bb52DAFa138764Be04Bc66","blockchains":["ethereum"]}]&defaultAsset=ETH&defaultPaymentMethod=CARD&fiatCurrency=USD&presetFiatAmount=10
+
+    // my url
+    // https://pay.coinbase.com/buy/select-asset?appId=aefedf19-488c-426c-b7be-133fab72807c&destinationWallets=[{"address":"0x652bd17D489F283A03bb52DAFa138764Be04Bc66","blockchains":["ethereum"]}]&defaultAsset=ETH&defaultPaymentMethod=CARD&fiatCurrency=USD&presetFiatAmount=10
+    // appId=aefedf19-488c-426c-b7be-133fab72807c
+
+    // https://pay.coinbase.com/buy?token=MWYwNDE3MjEtYTk1Mi02ZGNlLTg2MTEtOGVhNDZjN2QzNWRh
+    // https://pay.coinbase.com/buy?token=MWYwNDE3MmEtNGM1Yy02OGY4LWIxYjMtODIxNTE0ZDI3MTM5
+    // https://pay.coinbase.com/buy/select-asset?sessionToken=MWYwNDE3MmEtNGM1Yy02OGY4LWIxYjMtODIxNTE0ZDI3MTM5&destinationWallets=[{"address":"0x652bd17D489F283A03bb52DAFa138764Be04Bc66","blockchains":["ethereum"]}]&defaultAsset=ETH&defaultPaymentMethod=CARD&fiatCurrency=USD&presetFiatAmount=10
+    // https://pay.coinbase.com/buy/select-asset?appId=aefedf19-488c-426c-b7be-133fab72807c&destinationWallets=[{"address":"0x652bd17D489F283A03bb52DAFa138764Be04Bc66","blockchains":["ethereum"]}]&defaultAsset=ETH&defaultPaymentMethod=CARD&fiatCurrency=USD&presetFiatAmount=10
+
+    // https://pay.coinbase.com/buy/select-asset?appId=aefedf19-488c-426c-b7be-133fab72807c&addresses={"0x652bd17D489F283A03bb52DAFa138764Be04Bc66":["baseSepolia"]}&assets=["ETH"]
     if (isCoinbaseToggled) {
       generateCoinbaseUrl();
     } else {
@@ -88,6 +146,8 @@ export const OnRamp = () => {
             organizationId: session?.organizationId!,
             onrampProvider: "FIAT_ON_RAMP_PROVIDER_MOONPAY",
             transactionType: "FIAT_ON_RAMP_TRANSACTION_TYPE_BUY",
+            walletAddress: "0x652bd17D489F283A03bb52DAFa138764Be04Bc66",
+            fiatCurrencyCode: "USD",
           });
 
         console.log(
@@ -172,10 +232,8 @@ export const OnRamp = () => {
         <>
           {coinbaseOnRampBuyUrl && (
             <>
-              <FundButton
-                text={"Coinbase On Ramp"}
-                fundingUrl={coinbaseOnRampBuyUrl}
-              />
+              <FundButton text="Coinbase" fundingUrl={coinbaseOnRampBuyUrl} />
+              <Buy toToken={ethToken} />
             </>
           )}
         </>
