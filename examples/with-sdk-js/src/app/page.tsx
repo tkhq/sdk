5d05d11861894c264853be0971406de1ae82f9b8
Amir Cheikh <amir.cheikh@turnkey.io>
2025-06-06 15:57:37 -0400

    Init. Swap stampers by passing in enum

---
+++ examples/with-sdk-js/src/app/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./index.module.css";
+import { StamperType, TurnkeyClient } from "@turnkey/sdk-js";
+
+import { server } from "@turnkey/sdk-server";
+import { useEffect, useState } from "react";
+
+export default function AuthPage() {
+  const [client, setClient] = useState<TurnkeyClient | null>(null);
+
+  useEffect(() => {
+    const initializeClient = async () => {
+      const turnkeyClient = new TurnkeyClient({
+        apiBaseUrl: process.env.NEXT_PUBLIC_BASE_URL!,
+        organizationId: process.env.NEXT_PUBLIC_ORGANIZATION_ID!,
+        passkeyConfig: {
+          rpId: process.env.NEXT_PUBLIC_RPID!,
+          timeout: 60000, // 60 seconds
+          userVerification: "preferred",
+          allowCredentials: [],
+        },
+      });
+
+      await turnkeyClient.indexedDBStamper?.init();
+      setClient(turnkeyClient);
+    };
+
+    initializeClient();
+  }, []);
+
+  const passkey = async () => {
+    const resp = await client?.httpClient.getWhoami({}, StamperType.Passkey);
+    console.log("Response from getWhoami:", resp);
+  };
+
+  const indexedDB = async () => {
+    const resp = await client?.httpClient.getWhoami({});
+    console.log("Response from getWhoami:", resp);
+  };
+
+  return (
+    <main className={styles.main}>
+      <a
+        href="https://www.turnkey.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/logo.svg"
+          alt="Turnkey Logo"
+          className={styles.turnkeyLogo}
+          width={100}
+          height={24}
+          priority
+        />
+      </a>
+
+      <button
+        onClick={indexedDB}
+        style={{
+          backgroundColor: "green",
+          borderRadius: "8px",
+          padding: "8px 16px",
+          color: "white",
+        }}
+      >
+        IndexedDB
+      </button>
+
+      <button
+        onClick={passkey}
+        style={{
+          backgroundColor: "blue",
+          borderRadius: "8px",
+          padding: "8px 16px",
+          color: "white",
+        }}
+      >
+        Passkey
+      </button>
+    </main>
+  );
+}
