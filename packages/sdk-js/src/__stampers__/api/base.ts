39d5ed63e8705b2e20e87d4e90b3dffe2b2848d1
Amir Cheikh <amir.cheikh@turnkey.io>
2025-06-12 17:00:45 -0400

    Cross platform passkey stamper

--- packages/sdk-js/src/__stampers__/api/base.ts
+++ packages/sdk-js/src/__stampers__/api/base.ts
@@ -1,22 +1,20 @@
 import { isReactNative, isWeb } from "@utils";
 import { IndexedDbStamper } from "./web/stamper";
-import { StorageBase } from "../__storage__/base";
+import { StorageBase } from "../../__storage__/base";
+import { TStamp, TStamper } from "@types";
 
-export interface StamperBase {
+export interface ApiKeyStamperBase {
   listKeyPairs(): Promise<string[]>;
   createKeyPair(
     externalKeyPair?: CryptoKeyPair | { publicKey: string; privateKey: string },
   ): Promise<string>;
   deleteKeyPair(publicKeyHex: string): Promise<void>;
   clearKeyPairs(): Promise<void>;
-  stamp(
-    payload: string,
-    publicKeyHex: string,
-  ): Promise<{ stampHeaderName: string; stampHeaderValue: string }>;
+  stamp(payload: string, publicKeyHex: string): Promise<TStamp>;
 }
 
-export class CrossPlatformApiKeyStamper {
-  private stamper!: StamperBase;
+export class CrossPlatformApiKeyStamper implements TStamper {
+  private stamper!: ApiKeyStamperBase;
 
   constructor(private storageManager: StorageBase) {
     // Use init method to set up the stamper based on the platform. It's async, so can't be done in the constructor.
@@ -32,7 +30,7 @@ export class CrossPlatformApiKeyStamper {
         this.stamper = new ReactNativeKeychainStamper();
       } catch (error) {
         throw new Error(
-          `Failed to load storage manager for react-native: ${error}`,
+          `Failed to load keychain stamper for react-native: ${error}`,
         );
       }
     } else {
@@ -59,9 +57,7 @@ export class CrossPlatformApiKeyStamper {
     return this.stamper.clearKeyPairs();
   }
 
-  async stamp(
-    payload: string,
-  ): Promise<{ stampHeaderName: string; stampHeaderValue: string }> {
+  async stamp(payload: string): Promise<TStamp> {
     const session = await this.storageManager.getActiveSession();
     console.log("Active session:", session);
     console.log("Session token:", session?.token);
