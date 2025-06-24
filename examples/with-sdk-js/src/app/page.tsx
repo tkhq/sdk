6fb33c4b54248dc38b82369d6f50711a27fe7c51
Ethan Konkolowicz <ethan@turnkey.io>
2025-06-24 16:29:05 -0400

    added google oauth

--- examples/with-sdk-js/src/app/page.tsx
+++ examples/with-sdk-js/src/app/page.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import { Session, v1AddressFormat } from "@turnkey/sdk-types";
 import { init } from "next/dist/compiled/webpack/webpack";
 import { OtpType } from "@turnkey/sdk-js";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function AuthPage() {
   const [client, setClient] = useState<TurnkeyClient | null>(null);
@@ -149,8 +150,14 @@ export default function AuthPage() {
         passkeyName: `local-shmocal-passkey_${Date.now()}`,
       },
     });
+  };
 
-    console.log(res);
+  const handleGoogleLoginSuccess = async (credentialResponse: string) => {
+    const res = await client?.handleGoogleOauthLogin({
+      oidcToken: credentialResponse,
+    });
+
+    console.log("Google login response:", res);
   };
 
   const createWallet = async (walletName: string) => {
@@ -176,147 +183,133 @@ export default function AuthPage() {
 
   return (
     <main className={styles.main}>
-      <a
-        href="https://www.turnkey.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src="/logo.svg"
-          alt="Turnkey Logo"
-          className={styles.turnkeyLogo}
-          width={100}
-          height={24}
-          priority
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+        <a
+          href="https://www.turnkey.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/logo.svg"
+            alt="Turnkey Logo"
+            className={styles.turnkeyLogo}
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+
+        <GoogleLogin
+          onSuccess={async (credentialResponse) => {
+            if (!credentialResponse.credential) {
+              console.error("No credential received from Google login");
+              return;
+            }
+            await handleGoogleLoginSuccess(credentialResponse.credential);
+          }}
         />
-      </a>
-
-      <button
-        onClick={signUpWithPasskey}
-        style={{
-          backgroundColor: "rebeccapurple",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        Sign Up with Passkey
-      </button>
-
-      <input
-        type="text"
-        placeholder="Enter your email"
-        style={{
-          margin: "12px 0",
-          padding: "8px",
-          borderRadius: "4px",
-          border: "1px solid #ccc",
-          width: "300px",
-        }}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <button
-        onClick={initOtp}
-        style={{
-          backgroundColor: "rebeccapurple",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        Init OTP
-      </button>
-
-      <input
-        type="text"
-        placeholder="Enter OTP code"
-        style={{
-          margin: "12px 0",
-          padding: "8px",
-          borderRadius: "4px",
-          border: "1px solid #ccc",
-          width: "300px",
-        }}
-        onChange={(e) => {
-          setOtpCode(e.target.value);
-        }}
-      />
-
-      <button
-        onClick={() => verifyOtp()}
-        style={{
-          backgroundColor: "rebeccapurple",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        Verify OTP
-      </button>
-
-      <button
-        onClick={logout}
-        style={{
-          backgroundColor: "rosybrown",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        Logout
-      </button>
-
-      <button
-        onClick={indexedDB}
-        style={{
-          backgroundColor: "green",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        GetWhoami with IndexedDB
-      </button>
-
-      <button
-        onClick={createPasskey}
-        style={{
-          backgroundColor: "orange",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        Create Passkey
-      </button>
-      <button
-        onClick={logInWithPasskey1}
-        style={{
-          backgroundColor: "blue",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        Log in With Passkey Session 1
-      </button>
-      <button
-        onClick={logInWithPasskey2}
-        style={{
-          backgroundColor: "lightblue",
-          borderRadius: "8px",
-          padding: "8px 16px",
-          color: "white",
-        }}
-      >
-        Log in With Passkey Session 2
-      </button>
-
-      {client?.storageManager?.getActiveSession() ? (
         <button
-          onClick={getWallets}
+          onClick={signUpWithPasskey}
+          style={{
+            backgroundColor: "rebeccapurple",
+            borderRadius: "8px",
+            padding: "8px 16px",
+            color: "white",
+          }}
+        >
+          Sign Up with Passkey
+        </button>
+
+        <input
+          type="text"
+          placeholder="Enter your email"
+          style={{
+            margin: "12px 0",
+            padding: "8px",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+            width: "300px",
+          }}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <button
+          onClick={initOtp}
+          style={{
+            backgroundColor: "rebeccapurple",
+            borderRadius: "8px",
+            padding: "8px 16px",
+            color: "white",
+          }}
+        >
+          Init OTP
+        </button>
+
+        <input
+          type="text"
+          placeholder="Enter OTP code"
+          style={{
+            margin: "12px 0",
+            padding: "8px",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+            width: "300px",
+          }}
+          onChange={(e) => {
+            setOtpCode(e.target.value);
+          }}
+        />
+
+        <button
+          onClick={() => verifyOtp()}
+          style={{
+            backgroundColor: "rebeccapurple",
+            borderRadius: "8px",
+            padding: "8px 16px",
+            color: "white",
+          }}
+        >
+          Verify OTP
+        </button>
+
+        <button
+          onClick={logout}
+          style={{
+            backgroundColor: "rosybrown",
+            borderRadius: "8px",
+            padding: "8px 16px",
+            color: "white",
+          }}
+        >
+          Logout
+        </button>
+
+        <button
+          onClick={indexedDB}
+          style={{
+            backgroundColor: "green",
+            borderRadius: "8px",
+            padding: "8px 16px",
+            color: "white",
+          }}
+        >
+          GetWhoami with IndexedDB
+        </button>
+
+        <button
+          onClick={createPasskey}
+          style={{
+            backgroundColor: "orange",
+            borderRadius: "8px",
+            padding: "8px 16px",
+            color: "white",
+          }}
+        >
+          Create Passkey
+        </button>
+        <button
+          onClick={logInWithPasskey1}
           style={{
             backgroundColor: "blue",
             borderRadius: "8px",
@@ -324,27 +317,10 @@ export default function AuthPage() {
             color: "white",
           }}
         >
-          Get Wallets
+          Log in With Passkey Session 1
         </button>
-      ) : null}
-
-      {client?.storageManager?.getActiveSession() ? (
         <button
-          onClick={() => createWallet(`EVERYTHING ${wallets.length + 1}`)}
-          style={{
-            backgroundColor: "gray",
-            borderRadius: "8px",
-            padding: "8px 16px",
-            color: "white",
-          }}
-        >
-          Create Wallet
-        </button>
-      ) : null}
-
-      {client?.storageManager?.getActiveSession() ? (
-        <button
-          onClick={() => switchSession("session-1")}
+          onClick={logInWithPasskey2}
           style={{
             backgroundColor: "lightblue",
             borderRadius: "8px",
@@ -352,51 +328,93 @@ export default function AuthPage() {
             color: "white",
           }}
         >
-          Switch to Session 1
+          Log in With Passkey Session 2
         </button>
-      ) : null}
 
-      {client?.storageManager?.getActiveSession() ? (
-        <button
-          onClick={() => switchSession("session-2")}
-          style={{
-            backgroundColor: "lightblue",
-            borderRadius: "8px",
-            padding: "8px 16px",
-            color: "white",
-          }}
-        >
-          Switch to Session 2
-        </button>
-      ) : null}
+        {client?.storageManager?.getActiveSession() ? (
+          <button
+            onClick={getWallets}
+            style={{
+              backgroundColor: "blue",
+              borderRadius: "8px",
+              padding: "8px 16px",
+              color: "white",
+            }}
+          >
+            Get Wallets
+          </button>
+        ) : null}
 
-      {client?.storageManager?.getActiveSession() ? (
-        <button
-          onClick={getUser}
-          style={{
-            backgroundColor: "red",
-            borderRadius: "8px",
-            padding: "8px 16px",
-            color: "white",
-          }}
-        >
-          Get Users
-        </button>
-      ) : null}
+        {client?.storageManager?.getActiveSession() ? (
+          <button
+            onClick={() => createWallet(`EVERYTHING ${wallets.length + 1}`)}
+            style={{
+              backgroundColor: "gray",
+              borderRadius: "8px",
+              padding: "8px 16px",
+              color: "white",
+            }}
+          >
+            Create Wallet
+          </button>
+        ) : null}
 
-      {wallets.length > 0 && (
-        <button
-          onClick={signMessage}
-          style={{
-            backgroundColor: "pink",
-            borderRadius: "8px",
-            padding: "8px 16px",
-            color: "white",
-          }}
-        >
-          Sign Message
-        </button>
-      )}
+        {client?.storageManager?.getActiveSession() ? (
+          <button
+            onClick={() => switchSession("session-1")}
+            style={{
+              backgroundColor: "lightblue",
+              borderRadius: "8px",
+              padding: "8px 16px",
+              color: "white",
+            }}
+          >
+            Switch to Session 1
+          </button>
+        ) : null}
+
+        {client?.storageManager?.getActiveSession() ? (
+          <button
+            onClick={() => switchSession("session-2")}
+            style={{
+              backgroundColor: "lightblue",
+              borderRadius: "8px",
+              padding: "8px 16px",
+              color: "white",
+            }}
+          >
+            Switch to Session 2
+          </button>
+        ) : null}
+
+        {client?.storageManager?.getActiveSession() ? (
+          <button
+            onClick={getUser}
+            style={{
+              backgroundColor: "red",
+              borderRadius: "8px",
+              padding: "8px 16px",
+              color: "white",
+            }}
+          >
+            Get Users
+          </button>
+        ) : null}
+
+        {wallets.length > 0 && (
+          <button
+            onClick={signMessage}
+            style={{
+              backgroundColor: "pink",
+              borderRadius: "8px",
+              padding: "8px 16px",
+              color: "white",
+            }}
+          >
+            Sign Message
+          </button>
+        )}
+      </GoogleOAuthProvider>
     </main>
   );
 }
