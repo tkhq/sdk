deb2084fdbe088e11b938e630759d627f14907c3
Amir Cheikh <amir.cheikh@turnkey.io>
2025-06-25 18:19:19 -0400

    Iconbutton dark/light mode functionality

--- packages/react-wallet-kit/src/components/Buttons.tsx
+++ packages/react-wallet-kit/src/components/Buttons.tsx
@@ -1,21 +1,39 @@
-import { Button } from "@headlessui/react";
+import type { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { _internal_ComponentButton } from "@headlessui/react";
 
 interface IconButtonProps {
-  children: React.ReactNode;
+  icon: IconDefinition;
   onClick?: () => void;
   disabled?: boolean;
   className?: string;
 }
-export function IconButton(props: IconButtonProps) {
-  const { children, onClick, disabled, className } = props;
+
+export function BaseButton(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>,
+) {
+  const { children, className, disabled, ...buttonProps } = props;
 
   return (
-    <Button
-      className={`flex items-center justify-center p-2 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+    <button
+      className={`cursor-pointer border-none ${className || ""}`}
+      disabled={!!disabled}
+      {...buttonProps}
+    >
+      {children}
+    </button>
+  );
+}
+export function IconButton(props: IconButtonProps) {
+  const { icon, onClick, disabled, className } = props;
+  // TODO (Amir): Use standardized colors. Define these and make one for light and dark mode
+  return (
+    <BaseButton
+      className={`flex items-center justify-center p-2 rounded-full bg-gray-200 dark:bg-[#333336] ${className}`}
       onClick={onClick}
       disabled={!!disabled}
     >
-      {children}
-    </Button>
+      <FontAwesomeIcon icon={icon} />
+    </BaseButton>
   );
 }
