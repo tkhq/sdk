5c426a12043c8e9e56ca06f385af58de366cdf31
Amir Cheikh <amir.cheikh@turnkey.io>
2025-06-25 13:45:50 -0400

    Refactored modal. Icon button started

--- packages/react-wallet-kit/src/providers/modal/Root.tsx
+++ packages/react-wallet-kit/src/providers/modal/Root.tsx
@@ -6,6 +6,7 @@ import {
   TransitionChild,
 } from "@headlessui/react";
 import { useModal } from "./Provider";
+import { IconButton } from "../../components/Buttons";
 
 export function ModalRoot() {
   const { modalStack, popPage, closeModal } = useModal();
@@ -16,22 +17,26 @@ export function ModalRoot() {
   const innerPadding = 16;
 
   const containerRef = useRef<HTMLDivElement>(null);
-  const [height, setHeight] = useState<number>();
-  const [width, setWidth] = useState<number>();
+  const [height, setHeight] = useState<number>(300);
+  const [width, setWidth] = useState<number>(300);
 
   useEffect(() => {
     const resize = () => {
       if (containerRef.current) {
-        setContentBlur(10);
+        setContentBlur(10); // Blur the content during resize.
         const rect = containerRef.current.getBoundingClientRect();
+
         setHeight(rect.height);
         setWidth(rect.width);
-        setTimeout(() => setContentBlur(0), 100);
+        setTimeout(() => setContentBlur(0), 100); // Remove blur after resize
       }
     };
 
     if (current) {
       requestAnimationFrame(resize);
+    } else {
+      setHeight(height / 1.3);
+      setWidth(width / 1.3);
     }
   }, [current]);
 
@@ -54,10 +59,10 @@ export function ModalRoot() {
         {/* Modal Panel */}
         <div className="fixed inset-0 flex items-center justify-center">
           <DialogPanel>
-            {/* Modal content (absolute layer) */}
+            {/* White background container */}
             <TransitionChild
               as={Fragment}
-              enter="ease-out duration-200"
+              enter="ease-out duration-150"
               enterFrom="opacity-0"
               enterTo="opacity-100"
               leave="ease-in duration-150"
@@ -65,46 +70,24 @@ export function ModalRoot() {
               leaveTo="opacity-0"
             >
               <div
-                className="absolute z-10 transition-all duration-50"
-                style={{
-                  transform: `translate(${innerPadding}px, ${innerPadding}px)`,
-                  filter: `blur(${contentBlur}px)`,
-                }}
-                ref={containerRef}
-              >
-                {current?.content}
-              </div>
-            </TransitionChild>
-
-            {/* White background container */}
-            <TransitionChild
-              as={Fragment}
-              enter="ease-out duration-200"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-150"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <div
                 style={{
                   height,
                   width,
                   padding: innerPadding,
                 }}
-                className="bg-white rounded-2xl shadow-xl transition-all duration-200 ease-in-out"
+                className="bg-modal-background-light dark:bg-modal-background-dark flex rounded-2xl shadow-xl transition-all"
               >
                 <div
-                  className="absolute z-30 flex items-center justify-between transition-all duration-200 py-3"
+                  className="absolute z-30 flex items-center justify-between transition-all py-3"
                   style={{ width }}
                 >
                   {hasBack ? (
-                    <button
+                    <IconButton
                       onClick={popPage}
-                      className="text-sm text-gray-600 h-7 w-7"
+                      className="text-gray-600 h-7 w-7"
                     >
-                      ←
-                    </button>
+                      -
+                    </IconButton>
                   ) : (
                     <div />
                   )}
@@ -114,6 +97,16 @@ export function ModalRoot() {
                   >
                     ✕
                   </button>
+                </div>
+
+                <div
+                  className="z-10 transition-all duration-50 self-start"
+                  style={{
+                    filter: `blur(${contentBlur}px)`,
+                  }}
+                  ref={containerRef}
+                >
+                  {current?.content}
                 </div>
               </div>
             </TransitionChild>
