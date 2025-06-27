3b67f44af8c5bb53492f7d89578a93268bbf8c68
Amir Cheikh <amir.cheikh@turnkey.io>
2025-06-27 16:49:21 -0400

    Proper oauth flow in auth component. Spinners and design

--- packages/react-wallet-kit/rollup.config.mjs
+++ packages/react-wallet-kit/rollup.config.mjs
@@ -2,9 +2,13 @@ import postcss from "rollup-plugin-postcss";
 import typescript from "@rollup/plugin-typescript";
 import nodeExternals from "rollup-plugin-node-externals";
 import path from "node:path";
+import alias from "@rollup/plugin-alias";
+import url from "@rollup/plugin-url";
+
 
 const getFormatConfig = (format) => {
   const pkgPath = path.join(process.cwd(), "package.json");
+  const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
   /** @type {import('rollup').RollupOptions} */
   return {
@@ -17,6 +21,30 @@ const getFormatConfig = (format) => {
       sourcemap: true,
     },
     plugins: [
+      // Handle assets like images and fonts
+      url({
+        include: [
+          "**/*.svg",
+          "**/*.png",
+          "**/*.jpg",
+          "**/*.gif",
+          "**/*.woff",
+          "**/*.woff2",
+          "**/*.ttf",
+          "**/*.eot",
+        ],
+        limit: 8192,
+        emitFiles: true,
+        fileName: "assets/fonts/[name].[hash][extname]",
+      }),
+      alias({
+        entries: [
+          {
+            find: "assets",
+            replacement: path.resolve(__dirname, "src/assets"),
+          },
+        ],
+      }),
       typescript({
         tsconfig: "./tsconfig.json",
         outputToFilesystem: false,
