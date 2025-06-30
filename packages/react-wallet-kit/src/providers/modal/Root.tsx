05a62b03dd994597b799922d1d82c78155e87d95
Amir Cheikh <amir.cheikh@turnkey.io>
2025-06-30 17:25:48 -0400

    Email flow in auth component

--- packages/react-wallet-kit/src/providers/modal/Root.tsx
+++ packages/react-wallet-kit/src/providers/modal/Root.tsx
@@ -21,8 +21,35 @@ export function ModalRoot() {
   const containerRef = useRef<HTMLDivElement>(null);
   const [height, setHeight] = useState<number>(300);
   const [width, setWidth] = useState<number>(300);
+  const [observeResize, setObserveResize] = useState(true);
 
   useEffect(() => {
+    // This useEffect sets up a ResizeObserver to monitor changes in the size of the modal content.
+    // This only needs to run when the content is changing size without the page being swapped out
+    const node = containerRef.current;
+    if (!node || !observeResize) return;
+
+    const observer = new ResizeObserver(([entry]) => {
+      if (!entry) return;
+      setContentBlur(10);
+
+      const { width: newWidth, height: newHeight } = entry.contentRect;
+      setHeight(newHeight);
+      setWidth(newWidth);
+
+      setTimeout(() => setContentBlur(0), 100); // Remove blur after short delay
+    });
+
+    observer.observe(node);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [containerRef.current, observeResize]);
+
+  useEffect(() => {
+    // This useEffect handles the resizing of the modal when the current page changes.
+    // This only needs to run when the current page is changing. I.e, when the modal is pushed or popped.
     const resize = () => {
       if (containerRef.current) {
         setContentBlur(10); // Blur the content during resize.
@@ -35,8 +62,10 @@ export function ModalRoot() {
     };
 
     if (current) {
+      setObserveResize(true);
       requestAnimationFrame(resize);
     } else {
+      setObserveResize(false);
       setHeight(height / 1.3);
       setWidth(width / 1.3);
     }
@@ -106,16 +135,25 @@ export function ModalRoot() {
                     onClick={closeModal}
                   />
                 </div>
-
-                <div
-                  className="z-10 transition-all duration-50 self-start"
-                  style={{
-                    filter: `blur(${contentBlur}px)`,
-                  }}
-                  ref={containerRef}
+                <TransitionChild
+                  as={Fragment}
+                  enter="ease-in duration-200"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
                 >
-                  {current?.content}
-                </div>
+                  <div
+                    className="z-10 transition-all duration-50 self-start"
+                    style={{
+                      filter: `blur(${contentBlur}px)`,
+                    }}
+                    ref={containerRef}
+                  >
+                    {current?.content}
+                  </div>
+                </TransitionChild>
               </div>
             </TransitionChild>
           </DialogPanel>
