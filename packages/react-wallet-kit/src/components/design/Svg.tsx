959a0eec42454a335024b2ca031884034a1cb0fa
Amir Cheikh <amir.cheikh@turnkey.io>
2025-07-11 14:00:30 -0400

    Wallet login in auth component

--- packages/react-wallet-kit/src/components/design/Svg.tsx
+++ packages/react-wallet-kit/src/components/design/Svg.tsx
@@ -1,9 +1,9 @@
-interface TurnkeyLogoProps {
+interface SVGProps {
   className?: string;
   style?: React.CSSProperties;
 }
 
-export function TurnkeyLogo(props: TurnkeyLogoProps) {
+export function TurnkeyLogo(props: SVGProps) {
   return (
     <svg
       width="60"
@@ -13,7 +13,7 @@ export function TurnkeyLogo(props: TurnkeyLogoProps) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <g clip-path="url(#clip0_326_23361)">
+      <g clipPath="url(#clip0_326_23361)">
         <path
           fillRule="evenodd"
           clipRule="evenodd"
@@ -39,6 +39,131 @@ export function TurnkeyLogo(props: TurnkeyLogoProps) {
           />
         </clipPath>
       </defs>
+    </svg>
+  );
+}
+
+export function EthereumLogo(props: SVGProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlSpace="preserve"
+      id="Layer_1"
+      x={0}
+      y={0}
+      viewBox="0 0 327.5 533.3"
+      {...props}
+    >
+      <style>{".st0{fill:#8a92b2}.st1{fill:#62688f}"}</style>
+      <path d="M163.7 197.2V0L0 271.6l163.7-74.4z" className="st0" />
+      <path
+        d="M163.7 368.4V197.2L0 271.6l163.7 96.8zm0-171.2 163.7 74.4L163.7 0v197.2z"
+        className="st1"
+      />
+      <path
+        d="M163.7 197.2v171.2l163.7-96.8-163.7-74.4z"
+        style={{
+          fill: "#454a75",
+        }}
+      />
+      <path d="M163.7 399.4 0 302.7l163.7 230.7v-134z" className="st0" />
+      <path d="m327.5 302.7-163.8 96.7v134l163.8-230.7z" className="st1" />
+    </svg>
+  );
+}
+
+export function SolanaLogo(props: SVGProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlSpace="preserve"
+      viewBox="0 0 397.7 311.7"
+      {...props}
+    >
+      <linearGradient
+        id="a"
+        x1={360.879}
+        x2={141.213}
+        y1={351.455}
+        y2={-69.294}
+        gradientTransform="matrix(1 0 0 -1 0 314)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#00ffa3",
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#dc1fff",
+          }}
+        />
+      </linearGradient>
+      <path
+        d="M64.6 237.9c2.4-2.4 5.7-3.8 9.2-3.8h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1l62.7-62.7z"
+        style={{
+          fill: "url(#a)",
+        }}
+      />
+      <linearGradient
+        id="b"
+        x1={264.829}
+        x2={45.163}
+        y1={401.601}
+        y2={-19.148}
+        gradientTransform="matrix(1 0 0 -1 0 314)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#00ffa3",
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#dc1fff",
+          }}
+        />
+      </linearGradient>
+      <path
+        d="M64.6 3.8C67.1 1.4 70.4 0 73.8 0h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1L64.6 3.8z"
+        style={{
+          fill: "url(#b)",
+        }}
+      />
+      <linearGradient
+        id="c"
+        x1={312.548}
+        x2={92.882}
+        y1={376.688}
+        y2={-44.061}
+        gradientTransform="matrix(1 0 0 -1 0 314)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop
+          offset={0}
+          style={{
+            stopColor: "#00ffa3",
+          }}
+        />
+        <stop
+          offset={1}
+          style={{
+            stopColor: "#dc1fff",
+          }}
+        />
+      </linearGradient>
+      <path
+        d="M333.1 120.1c-2.4-2.4-5.7-3.8-9.2-3.8H6.5c-5.8 0-8.7 7-4.6 11.1l62.7 62.7c2.4 2.4 5.7 3.8 9.2 3.8h317.4c5.8 0 8.7-7 4.6-11.1l-62.7-62.7z"
+        style={{
+          fill: "url(#c)",
+        }}
+      />
     </svg>
   );
 }
