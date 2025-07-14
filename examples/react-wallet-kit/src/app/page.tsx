08c8d43369d1c107386f8e60e4d74c73a2ef548d
Amir Cheikh <amir.cheikh@turnkey.io>
2025-07-14 17:30:18 -0400

    Demo ewk config panel started

--- examples/react-wallet-kit/src/app/page.tsx
+++ examples/react-wallet-kit/src/app/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { useTurnkey } from "@turnkey/react-wallet-kit";
+import { useModal, useTurnkey } from "@turnkey/react-wallet-kit";
 
 export default function AuthPage() {
   const { login, handleExport } = useTurnkey();
 
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-yellow-400">
+    <div className="w-full h-screen flex items-center justify-center text-black">
       <div className="absolute top-0 right-0">demo</div>
-      <button onClick={login}>
-        <span className="text-white">One</span>
-      </button>
-      <button onClick={() => handleExport({ walletId: "4342" })}>
-        <span className="text-white">Two</span>
+      <button onClick={handleLogin}>
+        <span>One</span>
       </button>
     </div>
   );
