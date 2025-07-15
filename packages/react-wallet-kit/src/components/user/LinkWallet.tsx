f9aae4987f5f37c14c80aa0ef1da908b0f8ac716
Amir Cheikh <amir.cheikh@turnkey.io>
2025-07-14 20:07:16 -0400

    handeLinkWallet modal

---
+++ packages/react-wallet-kit/src/components/user/LinkWallet.tsx
@@ -0,0 +1,72 @@
+import { WalletProvider } from "@turnkey/wallet-stamper";
+import { ExternalWalletSelector, UnlinkWalletScreen } from "../auth/Wallet";
+import { useModal, useTurnkey } from "../../providers";
+import { ActionPage } from "../auth/Action";
+import { SuccessPage } from "../design/Success";
+
+interface LinkWalletModalProps {
+  providers: WalletProvider[];
+  successPageDuration?: number | undefined;
+}
+export function LinkWalletModal(props: LinkWalletModalProps) {
+  const { providers, successPageDuration } = props;
+  const { pushPage, closeModal } = useModal();
+  const { connectWalletAccount } = useTurnkey();
+
+  const hanldeLinkWallet = (provider: WalletProvider) => {
+    pushPage({
+      key: `Link ${provider.info.name}`,
+      content: (
+        <ActionPage
+          title={`Linking ${provider.info.name}`}
+          icon={
+            <img
+              className="size-11 rounded-full"
+              src={provider.info.icon || ""}
+            />
+          }
+          closeOnComplete={false}
+          action={async () => {
+            await connectWalletAccount(provider);
+            if (successPageDuration && successPageDuration > 0) {
+              pushPage({
+                key: "Link Success",
+                content: (
+                  <SuccessPage
+                    text="Successfully linked wallet!"
+                    onComplete={() => closeModal()}
+                    duration={successPageDuration}
+                  />
+                ),
+                showTitle: false,
+              });
+            } else {
+              closeModal();
+            }
+          }}
+        />
+      ),
+      showTitle: false,
+    });
+  };
+
+  const handleUnlinkWallet = (provider: WalletProvider) => {
+    pushPage({
+      key: `Unlink ${provider.info.name}`,
+      content: (
+        <UnlinkWalletScreen
+          provider={provider}
+          onUnlink={async () => console.log("unimplemented")}
+        />
+      ),
+      showTitle: false,
+    });
+  };
+  return (
+    <ExternalWalletSelector
+      providers={providers}
+      onSelect={hanldeLinkWallet}
+      onUnlink={handleUnlinkWallet}
+    />
+  );
+}
