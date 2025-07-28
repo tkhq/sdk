281c55aa8b9fe8b134c8a3775bc9d1ca4cd7a942
Amir Cheikh <amir.cheikh@turnkey.io>
2025-07-28 17:50:47 -0400

    Demo app lookin nice.

--- examples/react-wallet-kit/src/providers/config/ConfigProvider.tsx
+++ examples/react-wallet-kit/src/providers/config/ConfigProvider.tsx
@@ -8,8 +8,14 @@ import {
 import { TurnkeyConfigPanel } from "./Panel";
 import clsx from "clsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClose } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCaretDown,
+  faClose,
+} from "@fortawesome/free-solid-svg-icons";
 import { ThreeDimensionalBackground } from "@/components/3D/Background";
+import { TurnkeySVG } from "@/components/Svg";
+import { useScreenSize } from "@/utils";
 
 type ConfigContextValue = {
   config: TurnkeyProviderConfig;
@@ -36,69 +42,109 @@ export function TurnkeyConfigProvider({
   children,
 }: TurnkeyConfigProviderProps) {
   const [config, setConfigState] = useState(initialConfig);
-  const [panelOpen, setPanelOpen] = useState(false);
-
+  const [panelOpen, setPanelOpen] = useState(true);
+  const panelWidth = 384;
   const setConfig = (newConfig: Partial<TurnkeyProviderConfig>) => {
     setConfigState((prev) => ({ ...prev, ...newConfig }));
   };
 
+  const { isMobile } = useScreenSize();
   return (
     <ConfigContext.Provider value={{ config, setConfig }}>
       <div
         className={clsx(
-          "overflow-hidden h-full absolute inset-0 flex",
+          "overflow-hidden h-full absolute inset-0 flex dark:bg-panel-background-dark bg-panel-background-light text-text-light dark:text-text-dark",
           config.ui?.darkMode && "dark",
         )}
       >
         {/* Sliding Config Panel */}
         <Transition
           show={panelOpen}
-          enter="transition-transform duration-300"
+          enter="transition-transform duration-250 ease-out"
           enterFrom="-translate-x-full"
           enterTo="translate-x-0"
-          leave="transition-transform duration-300"
+          leave="transition-transform duration-150"
           leaveFrom="translate-x-0"
           leaveTo="-translate-x-full"
         >
-          <div className="z-20 fixed top-0 left-0 h-full w-96 bg-panel-background-light dark:bg-panel-background-dark border-r border-gray-300 dark:border-gray-700 text-text-light dark:text-text-dark shadow-lg flex flex-col px-4 py-6 space-y-4 transition-all">
+          <div className="z-20 fixed top-0 left-0 h-full w-full sm:w-96 bg-panel-background-light dark:bg-panel-background-dark flex flex-col px-4 py-6 space-y-4 transition-all">
             <div className="flex justify-between items-center mb-4">
-              <h2 className="text-lg font-semibold">Config Panel</h2>
+              <h2 className="text-lg font-semibold">Wallet Kit Config</h2>
               <Button
                 className="cursor-pointer"
                 onClick={() => setPanelOpen(false)}
               >
-                <FontAwesomeIcon icon={faClose} />
+                <FontAwesomeIcon size="lg" icon={faClose} />
               </Button>
             </div>
 
-            {/* Place for future toggles */}
-            <div className="flex flex-col space-y-3">
-              <TurnkeyConfigPanel />
-            </div>
+            <TurnkeyConfigPanel />
           </div>
         </Transition>
+
+        {!isMobile && (
+          <Button
+            onClick={() => setPanelOpen(!panelOpen)}
+            className={clsx(
+              "fixed z-10 cursor-pointer top-1/2 -translate-y-1/2 w-[80px] h-[250px] rounded-r-xl bg-panel-background-light dark:bg-panel-background-dark flex flex-col items-center justify-center transition-all ease-out",
+              panelOpen ? " -translate-x-full" : "",
+            )}
+          >
+            <div className="flex flex-row items-center justify-center gap-3 transform -rotate-90 text-xl font-medium text-center w-[250px] whitespace-pre">
+              <div className="size-4" />
+              <span>Config</span>
+              <FontAwesomeIcon className="size-4" icon={faCaretDown} />
+            </div>
+          </Button>
+        )}
 
         {/* Content (shifted when panel is open) */}
         <div
           style={{
-            transform: `translateX(${panelOpen ? "384px" : "0px"})`,
+            transform: `translateX(${panelOpen ? `${panelWidth / 2}px` : "0px"})`,
           }}
-          className={`flex-1 transition-all duration-300`}
+          className="flex-1 transition-all duration-150"
         >
           <TurnkeyProvider config={config} callbacks={callbacks}>
             <ThreeDimensionalBackground />
             <div className="relative z-10">{children}</div>
           </TurnkeyProvider>
-
-          <Button
-            onClick={() => setPanelOpen(!panelOpen)}
-            className="fixed z-50 cursor-pointer top-1/2 -translate-y-1/2 w-[80px] h-[250px] rounded-r-xl bg-panel-background-light dark:bg-panel-background-dark flex items-center justify-center"
-          >
-            <span className="transform -rotate-90 text-xl font-mediumtext-center w-[250px] whitespace-pre text-black dark:text-white">
-              Config
-            </span>
-          </Button>
         </div>
+        <div className="w-full z-10 h-14 sm:h-16 bg-panel-background-light backdrop-blur dark:bg-panel-background-dark absolute flex items-center justify-between px-2 sm:px-6.5">
+          {isMobile && (
+            <Button
+              onClick={() => setPanelOpen(!panelOpen)}
+              className="flex items-center justify-center p-2 gap-2 cursor-pointer text-xl"
+            >
+              <FontAwesomeIcon icon={faBars} />
+              Config
+            </Button>
+          )}
+
+          <TurnkeySVG
+            className="transition-all duration-250 size-32"
+            style={{
+              transform: `translateX(${panelOpen ? `${panelWidth}px` : "0px"})`,
+            }}
+          />
+
+          {!isMobile && (
+            <div className="flex font-bold text-sm w-52 justify-end">
+              <span>@turnkey/react-wallet-kit</span>
+            </div>
+          )}
+        </div>
+
+        {!isMobile && (
+          <>
+            <div className="absolute bottom-0 left-0 w-7 h-full bg-panel-background-light dark:bg-panel-background-dark flex items-center justify-center" />
+            <div className="absolute bottom-0 right-0 w-7 h-full bg-panel-background-light dark:bg-panel-background-dark flex items-center justify-center" />
+            <div className="absolute bottom-0 w-full h-7 bg-panel-background-light dark:bg-panel-background-dark flex items-center justify-center text-[8px] sm:text-xs text-icon-text-light/30 dark:text-icon-text-dark/30 font-extralight text-center">
+              This is purely a demo app for the Turnkey React Wallet Kit. You
+              will not receive any real tokens or rewards.
+            </div>
+          </>
+        )}
       </div>
     </ConfigContext.Provider>
   );
