942bff5f129de81261d104c13d474f768a8a500e
Amir Cheikh <amir.cheikh@turnkey.io>
2025-07-28 17:50:47 -0400

    Demo app lookin nice.

--- examples/react-wallet-kit/src/app/page.tsx
+++ examples/react-wallet-kit/src/app/page.tsx
@@ -1,16 +1,41 @@
 "use client";
 
-import { useModal, useTurnkey } from "@turnkey/react-wallet-kit";
+import { Spinner } from "@/components/Spinners";
+import { Button } from "@headlessui/react";
+import { ClientState, useTurnkey } from "@turnkey/react-wallet-kit";
+import { useEffect } from "react";
 
 export default function AuthPage() {
-  const { handleLogin, handleExport } = useTurnkey();
+  const { handleLogin, clientState } = useTurnkey();
+
+  useEffect(() => {
+    if (clientState === ClientState.Ready) {
+      handleLogin();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [clientState]);
 
   return (
-    <div className="w-full h-screen flex items-center justify-center text-black">
-      <div className="absolute top-0 right-0">demo</div>
-      <button onClick={handleLogin}>
-        <span>One</span>
-      </button>
+    <div className="w-full h-screen flex items-center justify-center">
+      {clientState === ClientState.Loading ? (
+        <Spinner className="size-48" strokeWidth={1} />
+      ) : clientState === ClientState.Error ? (
+        <Button
+          onClick={() => window.location.reload()}
+          className="w-full h-full flex justify-center items-center animate-pulse"
+        >
+          <span>
+            An error has occurred. Press anywhere to refresh your page
+          </span>
+        </Button>
+      ) : (
+        <Button
+          onClick={handleLogin}
+          className="w-full h-full flex justify-center items-center animate-pulse"
+        >
+          <span>Press anywhere to login</span>
+        </Button>
+      )}
     </div>
   );
 }
--- examples/react-wallet-kit/src/components/3D/Background.tsx
+++ examples/react-wallet-kit/src/components/3D/Background.tsx
@@ -1,16 +1,54 @@
 import { Canvas } from "@react-three/fiber";
 import { TurnkeyLogo } from "./TurnkeyLogo";
 import { Stars } from "./Stars";
+import { PerspectiveCamera, useProgress } from "@react-three/drei";
+import { Suspense, useEffect, useState } from "react";
+import clsx from "clsx";
+import { useScreenSize } from "@/utils";
+
+function FadeInWrapper(props: { children: React.ReactNode }) {
+  const { children } = props;
+  const { progress } = useProgress(); // This tracks loading progress of async assets like the Turnkey glb model
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (progress === 100) {
+      const timeout = setTimeout(() => setVisible(true), 50); // Fade in after a short delay
+      return () => clearTimeout(timeout);
+    }
+  }, [progress]);
+
+  return (
+    <div
+      className={clsx(
+        "absolute inset-0 w-full overflow-hidden pointer-events-none bg-background-light dark:bg-background-dark transition-opacity duration-1000",
+        visible ? "opacity-100" : "opacity-0",
+      )}
+    >
+      {children}
+    </div>
+  );
+}
 
 export function ThreeDimensionalBackground() {
+  const { isMobile } = useScreenSize();
+
   return (
-    <div className="absolute inset-0 w-full overflow-hidden pointer-events-none bg-background-light dark:bg-background-dark">
+    <FadeInWrapper>
       <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[0, 0, 5]} color="red" />
-        <Stars position={[0, 0, 3]} />
-        <TurnkeyLogo position={[1, -2, 3]} />
+        <Suspense fallback={null}>
+          <PerspectiveCamera
+            makeDefault
+            position={[isMobile ? 0.5 : 0, 0, isMobile ? 5 : 4.5]}
+            rotation={[-0.2, -0.1, -0.2]}
+            near={0.1}
+            far={1000}
+          />
+          <ambientLight intensity={0.1} />
+          <Stars position={[0, 0, 4.5]} />
+          <TurnkeyLogo position={[1, -2.5, 2]} />
+        </Suspense>
       </Canvas>
-    </div>
+    </FadeInWrapper>
   );
 }
