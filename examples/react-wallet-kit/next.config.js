6b788c0140b8988f54b74c61b3d56aa8ddc0cb0b
Ethan Konkolowicz <ethan@turnkey.io>
2025-08-05 17:20:43 -0400

    more demo polish

--- examples/react-wallet-kit/next.config.js
+++ examples/react-wallet-kit/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
+  images: {
+    domains: [],
+    unoptimized: true, // Allow base64-encoded images
+  },
 };
 
 module.exports = nextConfig;
