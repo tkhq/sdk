44d98b1f83dc214201f8f6d81574af7a08b80450
Amir Cheikh <amir.cheikh@turnkey.io>
2025-08-07 10:38:47 -0400

    Node >18 fix

--- packages/react-wallet-kit/src/components/auth/Wallet.tsx
+++ packages/react-wallet-kit/src/components/auth/Wallet.tsx
@@ -352,9 +352,9 @@ export function ConnectedIndicator(props: ConnectedIndicatorProps) {
   return (
     <div className="flex absolute top-[-2px] right-0">
       {isPinging && (
-        <div className="absolute animate-ping size-[6px] bg-green-500 rounded-full border border-modal-background-light dark:border-modal-background-dark" />
+        <div className="absolute animate-ping size-[6px] bg-success-light dark:bg-success-dark rounded-full border border-modal-background-light dark:border-modal-background-dark" />
       )}
-      <div className="size-[6px] bg-green-500 rounded-full border border-modal-background-light dark:border-modal-background-dark" />
+      <div className="size-[6px] bg-success-light dark:bg-success-dark rounded-full border border-modal-background-light dark:border-modal-background-dark" />
     </div>
   );
 }
