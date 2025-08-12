b3120f68fdbc0d008305d1e53f2af15363110f11
Amir Cheikh <amir.cheikh@turnkey.io>
2025-08-12 11:52:42 -0400

    Resolved comments

---
+++ examples/react-wallet-kit/src/constants.ts
@@ -0,0 +1,95 @@
+import {
+  CreateSubOrgParams,
+  TurnkeyProviderConfig,
+} from "@turnkey/react-wallet-kit";
+
+export const createSuborgParams: CreateSubOrgParams = {
+  customWallet: {
+    walletName: "Wallet 1",
+    walletAccounts: [
+      {
+        addressFormat: "ADDRESS_FORMAT_ETHEREUM",
+        curve: "CURVE_SECP256K1",
+        pathFormat: "PATH_FORMAT_BIP32",
+        path: "m/44'/60'/0'/0/0",
+      },
+      {
+        addressFormat: "ADDRESS_FORMAT_SOLANA",
+        curve: "CURVE_ED25519",
+        pathFormat: "PATH_FORMAT_BIP32",
+        path: "m/44'/501'/0'/0/0",
+      },
+    ],
+  },
+};
+
+export const initialConfig: TurnkeyProviderConfig = {
+  apiBaseUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://api.turnkey.com",
+  authProxyUrl:
+    process.env.NEXT_PUBLIC_AUTH_PROXY_URL || "https://authproxy.turnkey.com",
+  authProxyId: process.env.NEXT_PUBLIC_AUTH_PROXY_ID!,
+  organizationId: process.env.NEXT_PUBLIC_ORGANIZATION_ID!,
+  importIframeUrl:
+    process.env.NEXT_PUBLIC_IMPORT_IFRAME_URL || "https://import.turnkey.com",
+  exportIframeUrl:
+    process.env.NEXT_PUBLIC_EXPORT_IFRAME_URL || "https://export.turnkey.com",
+  auth: {
+    oAuthConfig: {
+      googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      appleClientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
+      facebookClientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
+      oAuthRedirectUri: process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI,
+      openOAuthInPage: true,
+    },
+    methods: {
+      emailOtpAuthEnabled: true,
+      smsOtpAuthEnabled: false,
+      passkeyAuthEnabled: true,
+      walletAuthEnabled: true,
+      googleOAuthEnabled: true,
+      appleOAuthEnabled: false,
+      facebookOAuthEnabled: false,
+    },
+    methodOrder: ["socials", "email", "sms", "passkey", "wallet"],
+    oauthOrder: ["google", "apple", "facebook"],
+    autoRefreshSession: true,
+    createSuborgParams: {
+      emailOtpAuth: createSuborgParams,
+      smsOtpAuth: createSuborgParams,
+      passkeyAuth: createSuborgParams,
+      walletAuth: createSuborgParams,
+      oAuth: createSuborgParams,
+    },
+  },
+  ui: {
+    darkMode: true,
+    borderRadius: 16,
+    backgroundBlur: 8,
+    renderModalInProvider: true, // This is needed for the config panel to push the modal over
+    colors: {
+      light: {
+        primary: "#335bf9",
+        modalBackground: "#f5f7fb",
+      },
+      dark: {
+        primary: "#335bf9",
+        modalBackground: "#0b0b0b",
+      },
+    },
+  },
+  walletConfig: {
+    ethereum: true,
+    solana: true,
+    walletConnect: {
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+      metadata: {
+        name: "Turnkey Wallet",
+        description: "A wallet for Turnkey",
+        url: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_URL!,
+        icons: ["/favicon.svg"],
+      },
+      ethereumNamespaces: ["eip155:1"],
+      solanaNamespaces: ["solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp"],
+    },
+  },
+};
