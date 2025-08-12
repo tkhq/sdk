86c82331a0620c6f605793f70e6fd254eba48b26
Mohammed Odeh <moe.odeh@turnkey.io>
2025-08-12 10:03:35 -0400

    enable mobile

--- packages/core/src/__wallet__/base.ts
+++ packages/core/src/__wallet__/base.ts
@@ -1,22 +1,15 @@
 import { WebWalletManager } from "./web/manager";
 import { isReactNative, isWeb } from "@utils";
 import type { TWalletManagerConfig, WalletManagerBase } from "@types";
+import { MobileWalletManager } from "./mobile/manager";
 
 export async function createWalletManager(
   cfg: TWalletManagerConfig,
 ): Promise<WalletManagerBase> {
   if (isReactNative()) {
-    try {
-      // const { MobileWalletManager } = await import("../mobile/base");
-      // return new MobileWalletManager(cfg);
-      throw new Error(
-        "WalletManager is not yet implemented for React Native. Please use WebWalletManager.",
-      );
-    } catch (error) {
-      throw new Error(
-        `Failed to load wallet manager for react-native: ${error}`,
-      );
-    }
+    const manager = new MobileWalletManager(cfg);
+    await manager.init(cfg);
+    return manager;
   } else if (isWeb()) {
     const manager = new WebWalletManager(cfg);
     await manager.init(cfg);
