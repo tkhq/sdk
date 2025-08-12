5e2b12c9ee687da6d254d84baa5d02c0b10c8ea5
Ethan Konkolowicz <ethan@turnkey.io>
2025-08-12 11:37:25 -0400

    fixed comments made by bryce and andrew for demo

--- examples/react-wallet-kit/src/components/demo/AuthButtons/AuthenticatorButton.tsx
+++ examples/react-wallet-kit/src/components/demo/AuthButtons/AuthenticatorButton.tsx
@@ -1,10 +1,18 @@
 import { KeySVG } from "@/components/Svg";
 import { AuthToggleButton } from "./index";
 import { useTurnkey } from "@turnkey/react-wallet-kit";
+import { toast } from "react-toastify";
 
-export default function AuthenticatorButton() {
+export default function AuthenticatorButton({
+  canRemoveAuthMethod,
+}: {
+  canRemoveAuthMethod: boolean;
+}) {
   const { user, handleAddPasskey, handleRemovePasskey } = useTurnkey();
   const authenticator = user?.authenticators?.[0];
+
+  const notify = () =>
+    toast.error("Error: Cannot remove your last authenticator.");
 
   return (
     <AuthToggleButton
@@ -12,11 +20,12 @@ export default function AuthenticatorButton() {
       icon={<KeySVG className="w-6 h-6" />}
       isLinked={!!authenticator}
       onAdd={handleAddPasskey}
-      onRemove={() =>
+      canRemoveAuthMethod={canRemoveAuthMethod}
+      onRemove={() => {
         handleRemovePasskey({
           authenticatorId: authenticator?.authenticatorId!,
-        })
-      }
+        });
+      }}
     />
   );
 }
