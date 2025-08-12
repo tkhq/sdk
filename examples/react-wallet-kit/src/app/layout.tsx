6afef25ab327f52625bac72b394ee5ec38085eba
Amir Cheikh <amir.cheikh@turnkey.io>
2025-08-12 11:58:01 -0400

    Rebase fixes

--- examples/react-wallet-kit/src/app/layout.tsx
+++ examples/react-wallet-kit/src/app/layout.tsx
@@ -34,99 +34,14 @@ function RootLayout({ children }: RootLayoutProps) {
       </head>
       <body className="bg-background-light dark:bg-background-dark">
         <TurnkeyConfigProvider
-          initialConfig={{
-            apiBaseUrl:
-              process.env.NEXT_PUBLIC_BASE_URL || "https://api.turnkey.com",
-            authProxyUrl:
-              process.env.NEXT_PUBLIC_AUTH_PROXY_URL ||
-              "https://authproxy.turnkey.com",
-            authProxyId: process.env.NEXT_PUBLIC_AUTH_PROXY_ID!,
-            organizationId: process.env.NEXT_PUBLIC_ORGANIZATION_ID!,
-            importIframeUrl:
-              process.env.NEXT_PUBLIC_IMPORT_IFRAME_URL ||
-              "https://import.turnkey.com",
-            exportIframeUrl:
-              process.env.NEXT_PUBLIC_EXPORT_IFRAME_URL ||
-              "https://export.turnkey.com",
-            auth: {
-              oAuthConfig: {
-                googleClientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-                appleClientId: process.env.NEXT_PUBLIC_APPLE_CLIENT_ID,
-                facebookClientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
-                oAuthRedirectUri: process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI,
-                openOAuthInPage: true,
-              },
-              methods: {
-                emailOtpAuthEnabled: true,
-                smsOtpAuthEnabled: false,
-                passkeyAuthEnabled: true,
-                walletAuthEnabled: true,
-                googleOAuthEnabled: true,
-                appleOAuthEnabled: false,
-                facebookOAuthEnabled: false,
-              },
-              methodOrder: ["socials", "email", "sms", "passkey", "wallet"],
-              oauthOrder: ["google", "apple", "facebook"],
-              autoRefreshSession: true,
-              createSuborgParams: {
-                emailOtpAuth: createSuborgParams,
-                smsOtpAuth: createSuborgParams,
-                passkeyAuth: createSuborgParams,
-                walletAuth: createSuborgParams,
-                oAuth: createSuborgParams,
-              },
-            },
-            ui: {
-              darkMode: true,
-              borderRadius: 16,
-              backgroundBlur: 8,
-              renderModalInProvider: true, // This is needed for the config panel to push the modal over
-              colors: {
-                light: {
-                  primary: "#335bf9",
-                  modalBackground: "#f5f7fb",
-                },
-                dark: {
-                  primary: "#335bf9",
-                  modalBackground: "#0b0b0b",
-                },
-              },
-            },
-            walletConfig: {
-              features: {
-                auth: true,
-                connecting: true,
-              },
-              chains: {
-                ethereum: {
-                  native: true,
-                  walletConnectNamespaces: ["eip155:1"],
-                },
-                solana: {
-                  native: true,
-                  walletConnectNamespaces: [
-                    "solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp",
-                  ],
-                },
-              },
-              walletConnect: {
-                projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
-                appMetadata: {
-                  name: "Turnkey Wallet",
-                  description: "A wallet for Turnkey",
-                  url: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_URL!,
-                  icons: ["/favicon.svg"],
-                },
-              },
-            },
-          }}
+          initialConfig={initialConfig}
           callbacks={{
             onError: (error) => {
               console.error("Turnkey Error:", error.code);
               switch (error.code) {
                 case TurnkeyErrorCodes.ACCOUNT_ALREADY_EXISTS:
                   notify(
-                    "This social login is already associated with another account."
+                    "This social login is already associated with another account.",
                   );
                   break;
                 default:
