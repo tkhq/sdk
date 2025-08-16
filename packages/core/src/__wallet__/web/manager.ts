06cceaa88975cca5dc28fcc85fcd2ea1a44483b1
Mohammed Odeh <moe.odeh@turnkey.io>
2025-08-16 18:08:32 -0400

    debug logs

--- packages/core/src/__wallet__/web/manager.ts
+++ packages/core/src/__wallet__/web/manager.ts
@@ -89,6 +89,7 @@ export class WebWalletManager {
       this.stamper = new CrossPlatformWalletStamper(this.wallets);
     }
 
+    console.log("config: ", cfg);
     if (cfg.features?.connecting) {
       this.connector = new CrossPlatformWalletConnector(this.wallets);
     }
