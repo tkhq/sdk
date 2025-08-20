7576942efcd67adbfad651eb7bed58fb74e8162f
Mohammed Odeh <moe.odeh@turnkey.io>
2025-08-20 17:17:52 -0400

    fix initialization of event listeners for connected wallet providers

--- packages/core/src/__clients__/core.ts
+++ packages/core/src/__clients__/core.ts
@@ -1646,9 +1646,10 @@ export class TurnkeyClient {
    * @throws {TurnkeyError} If no active session is found or if there is an error fetching wallets.
    */
   fetchWallets = async (params?: {
+    walletProviders?: WalletProvider[] | undefined;
     stampWith?: StamperType | undefined;
   }): Promise<Wallet[]> => {
-    const { stampWith } = params || {};
+    const { stampWith, walletProviders } = params || {};
     const session = await this.storageManager.getActiveSession();
 
     if (!session) {
@@ -1693,7 +1694,7 @@ export class TurnkeyClient {
         // if wallet connecting is disabled we return only embedded wallets
         if (!this.walletManager?.connector) return embedded;
 
-        const providers = await this.getWalletProviders();
+        const providers = walletProviders ?? (await this.getWalletProviders());
 
         const groupedProviders = new Map<string, WalletProvider[]>();
         for (const provider of providers) {
