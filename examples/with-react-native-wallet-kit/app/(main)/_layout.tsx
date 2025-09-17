c7442d1e8dccbf801ac2839cf23b06870d7477f3
Taylor Dawson <taylor@turnkey.io>
2025-09-17 12:57:04 -0700

    WIP: commit to deploy codesandbox

--- examples/with-react-native-wallet-kit/app/(main)/_layout.tsx
+++ examples/with-react-native-wallet-kit/app/(main)/_layout.tsx
@@ -1,5 +1,4 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
 
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
--- packages/react-native-wallet-kit/src/index.ts
+++ packages/react-native-wallet-kit/src/index.ts
@@ -1,8 +1,6 @@
 // React Native Wallet Kit - Main exports
 export * from './types';
-export * from './providers';
-export * from './components';
-export * from './utils';
 
-// Re-export the main provider for convenience
+export { useTurnkey } from './hooks/use-turnkey';
+
 export { TurnkeyProvider } from './providers';
