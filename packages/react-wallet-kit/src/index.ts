55168a693598d3262b2c12fee095132b4722b177
Mohammed Odeh <moe.odeh@turnkey.io>
2025-09-22 12:52:56 -0600

    test 2

--- packages/react-wallet-kit/src/index.ts
+++ packages/react-wallet-kit/src/index.ts
@@ -2,6 +2,6 @@
 import "./index.css";
 export * from "./providers";
 /** @internal */
-export * from "@turnkey/core/dist/__types__/index";
+export * from "@turnkey/core/dist/__types__/auth";
 export * from "./types/base";
 export * from "./types/method-types";
