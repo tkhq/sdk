1159a1806be4645249fc432a03cf643907b3a0d2
Narimon Farhangi <narimon@turnkey.io>
2025-09-25 16:04:29 -0400

    review

--- packages/crypto/src/proof.ts
+++ packages/crypto/src/proof.ts
@@ -6,9 +6,18 @@ import * as CBOR from "cbor-js";
 import * as x509 from "@peculiar/x509";
 import { Crypto as PeculiarCrypto } from "@peculiar/webcrypto";
 import { createHash, createPublicKey, createVerify } from "node:crypto";
+import { AWS_ROOT_CERT_PEM, AWS_ROOT_CERT_SHA256 } from "./constants";
 
 x509.cryptoProvider.set(new PeculiarCrypto());
 
+// verify goes through the following verification steps for an app proof & boot proof pair:
+//  - Verify app proof signature
+//  - Verify the boot proof
+//    - Attestation doc was signed by AWS
+//    - Attestation doc's `user_data` is the hash of the qos manifest
+//  - Verify the connection between the app proof & boot proof i.e. that the ephemeral key's match
+//
+// For more information, check out https://whitepaper.turnkey.com/foundations
 export async function verify(
   appProof: v1AppProof,
   bootProof: v1BootProof,
@@ -132,26 +141,6 @@ export function verifyAppProofSignature(appProof: v1AppProof): void {
   }
 }
 
-// Pinned AWS Nitro Enclaves Root
-const AWS_ROOT_CERT_PEM = `-----BEGIN CERTIFICATE-----
-MIICETCCAZagAwIBAgIRAPkxdWgbkK/hHUbMtOTn+FYwCgYIKoZIzj0EAwMwSTEL
-MAkGA1UEBhMCVVMxDzANBgNVBAoMBkFtYXpvbjEMMAoGA1UECwwDQVdTMRswGQYD
-VQQDDBJhd3Mubml0cm8tZW5jbGF2ZXMwHhcNMTkxMDI4MTMyODA1WhcNNDkxMDI4
-MTQyODA1WjBJMQswCQYDVQQGEwJVUzEPMA0GA1UECgwGQW1hem9uMQwwCgYDVQQL
-DANBV1MxGzAZBgNVBAMMEmF3cy5uaXRyby1lbmNsYXZlczB2MBAGByqGSM49AgEG
-BSuBBAAiA2IABPwCVOumCMHzaHDimtqQvkY4MpJzbolL//Zy2YlES1BR5TSksfbb
-48C8WBoyt7F2Bw7eEtaaP+ohG2bnUs990d0JX28TcPQXCEPZ3BABIeTPYwEoCWZE
-h8l5YoQwTcU/9KNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUkCW1DdkF
-R+eWw5b6cp3PmanfS5YwDgYDVR0PAQH/BAQDAgGGMAoGCCqGSM49BAMDA2kAMGYC
-MQCjfy+Rocm9Xue4YnwWmNJVA44fA0P5W2OpYow9OYCVRaEevL8uO1XYru5xtMPW
-rfMCMQCi85sWBbJwKKXdS6BptQFuZbT73o/gBh1qUxl/nNr12UO8Yfwr6wPLb+6N
-IwLz3/Y=
------END CERTIFICATE-----`;
-
-// Official SHA-256 fingerprint
-const AWS_ROOT_CERT_SHA256 =
-  "641A0321A3E244EFE456463195D606317ED7CDCC3C1756E09893F3C68F79BB5B";
-
 export async function verifyCertificateChain(
   cabundle: Uint8Array[],
   rootCertPem: string,
@@ -184,18 +173,21 @@ export async function verifyCertificateChain(
     const builder = new x509.X509ChainBuilder({
       certificates: [rootX509, ...intermediatesX509],
     });
-    const path = await builder.build(leaf);
-    if (!path || path.length < 2)
-      throw new Error("Could not build a certificate path to the AWS root");
+    const chain = await builder.build(leaf);
+    if (chain.length !== intermediatesX509.length + 2)
+      throw new Error(
+        `Incorrect number of certs in X509 Chain. Expected ${intermediatesX509.length + 2}, got ${chain.length}`,
+      );
 
-    // Verify certificate chain signatures
+    // It appears `builder.build` does signature-only verification of the chain, but there's no commitement to that in the
+    // public api, so we loop through the chain and do (maybe redundant) signature verification
     const appProofDate = new Date(timestampMs);
-    for (let i = 0; i < path.length; i++) {
-      const cert = path[i];
+    for (let i = 0; i < chain.length; i++) {
+      const cert = chain[i];
       if (!cert) throw new Error("Invalid certificate in chain");
 
       // Verify signature
-      if (i === path.length - 1) {
+      if (i === chain.length - 1) {
         // is root
         // Self-signature verification for root certificate
         const ok = await cert.verify({
@@ -206,12 +198,11 @@ export async function verifyCertificateChain(
           throw new Error("Pinned root failed self-signature verification");
       } else {
         // Verify signature against issuer
-        const issuer = path[i + 1];
+        const issuer = chain[i + 1];
         if (!issuer) throw new Error("Issuer can't be null");
 
         // Attestation docs technically expire after 3 hours, so an app proof generated 3+ hours after an enclave
         // boots up will fail verification due to certificate expiration. To prevent this failure, we set `signatureOnly: true`.
-        // We've scoped work to solve this by generating attestation docs every couple hours, coming soon.
         const ok = await cert.verify({
           publicKey: issuer.publicKey,
           signatureOnly: true,
