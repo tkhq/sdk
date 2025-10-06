da2ce244058ec43cf799c77da056e83c190ca167
Taylor Dawson <taylor@turnkey.io>
2025-10-06 14:26:06 -0700

    switch to preprod url for testing

--- packages/react-native-wallet-kit/src/utils/utils.ts
+++ packages/react-native-wallet-kit/src/utils/utils.ts
@@ -17,8 +17,8 @@ export const FACEBOOK_AUTH_URL = "https://www.facebook.com/v23.0/dialog/oauth";
 export const FACEBOOK_GRAPH_URL =
   "https://graph.facebook.com/v23.0/oauth/access_token";
 //@todo remove, local testing only
-export const TURNKEY_OAUTH_ORIGIN_URL =  "http://localhost:3000" //"https://oauth-origin.turnkey.com";
-export const TURNKEY_OAUTH_REDIRECT_URL = "https://oauth-redirect.turnkey.com";
+export const TURNKEY_OAUTH_ORIGIN_URL =  "https://oauth-origin.preprod.turnkey.engineering" //"https://oauth-origin.turnkey.com";
+export const TURNKEY_OAUTH_REDIRECT_URL = "https://oauth-redirect.preprod.turnkey.engineering";
 
 export const SESSION_WARNING_THRESHOLD_MS = 60 * 1000; // 1 minute in milliseconds
 
