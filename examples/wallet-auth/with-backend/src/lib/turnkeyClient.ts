fd0aadc331c680e9a8d8c1e207d2cba8ee802b1f
Traian Vila <traian@turnkey.io>
2025-10-30 15:35:27 +0200

    drop lib

--- examples/wallet-auth/with-backend/src/lib/turnkeyClient.ts
+++
@@ -1,9 +0,0 @@
-// lib/turnkeyClient.ts
-import { TurnkeyClient } from "@turnkey/core";
-
-export const turnkey = new TurnkeyClient({
-  organizationId: process.env.NEXT_PUBLIC_ORGANIZATION_ID!, // must be defined
-});
-
-// Initialize once. Safe to call multiple times; subsequent calls are no-ops.
-void turnkey.init();
