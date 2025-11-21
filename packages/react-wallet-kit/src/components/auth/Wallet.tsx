558949c083c3cd90bee739f47d80ca5232d45cc3
Mohammed Odeh <moe.odeh@turnkey.io>
2025-11-21 11:04:16 -0500

    rebased

--- packages/react-wallet-kit/src/components/auth/Wallet.tsx
+++ packages/react-wallet-kit/src/components/auth/Wallet.tsx
@@ -438,6 +438,7 @@ function QRCodeDisplay(props: QRCodeDisplayProps) {
 
   return (
     <div className="relative inline-block">
+      {/* @ts-expect-error: qrcode.react uses a different React type version */}
       <QRCode
         className={clsx(
           "block border border-modal-background-dark/20 dark:border-modal-background-light/20",
