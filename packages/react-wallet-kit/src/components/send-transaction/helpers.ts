138f3e7ff0b6e839aaab9535e5679195ced074b4
Mohammad Cheikh <moe@turnkey.io>
2025-11-26 11:51:45 -0500

    working sponsored tx e2e

--- packages/react-wallet-kit/src/components/send-transaction/helpers.ts
+++ packages/react-wallet-kit/src/components/send-transaction/helpers.ts
@@ -44,3 +44,32 @@ export async function generateNonces({
 
   return { nonce };
 }
+
+export function getExplorerUrl(txHash: string, caip2: string): string {
+  switch (caip2) {
+    case "eip155:8453":
+      return `https://basescan.org/tx/${txHash}`;
+    case "eip155:84532":
+      return `https://sepolia.basescan.org/tx/${txHash}`;
+    case "eip155:1":
+      return `https://etherscan.io/tx/${txHash}`;
+    case "eip155:11155111":
+      return `https://sepolia.etherscan.io/tx/${txHash}`;
+    case "eip155:137":
+      return `https://polygonscan.com/tx/${txHash}`;
+    default:
+      return `https://etherscan.io/tx/${txHash}`;
+  }
+}
+
+export function getChainLogo(caip2: string): string {
+  if (caip2.startsWith("eip155:")) {
+    return "https://cryptologos.cc/logos/ethereum-eth-logo.svg";
+  }
+
+  if (caip2.startsWith("solana:")) {
+    return "https://cryptologos.cc/logos/solana-sol-logo.svg";
+  }
+
+  return "https://cryptologos.cc/logos/ethereum-eth-logo.svg";
+}
