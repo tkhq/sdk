cdba317097c35fff70fa7380b07a1b85e4f843ff
Mohammad Cheikh <moe@turnkey.io>
2025-12-05 11:48:21 -0500

    add optional org id params

--- packages/core/src/__types__/method-types/shared.ts
+++ packages/core/src/__types__/method-types/shared.ts
@@ -236,13 +236,15 @@ export type SignAndSendRawTransactionParams = {
 };
 
 export type PollTransactionStatusParams = {
-  organizationId: string;
+  organizationId?: string;
+  stampWith?: StamperType | undefined;
   sendTransactionStatusId: string;
   pollingIntervalMs?: number;
 };
 
 export type SignAndSendTransactionParams = {
   organizationId?: string | undefined;
+  stampWith?: StamperType | undefined;
   from: string;
   sponsor?: boolean | undefined;
   caip2: string;
