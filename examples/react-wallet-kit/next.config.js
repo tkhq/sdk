8544d0845de37ac01e6ce5def72e8d3da80f1a82
Rayan Fawaz <rayanfawaz@Rayans-MacBook-Pro.local>
2026-01-01 14:36:39 -0500

    added next bundle anaylzerand improve tree-shaking and exclude non-runtime files

--- examples/react-wallet-kit/next.config.js
+++ examples/react-wallet-kit/next.config.js
@@ -1,3 +1,7 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
@@ -7,4 +11,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
