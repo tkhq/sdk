9d8bb1d22518c67b1e4d854068095d61e8a9ca42
Amir Cheikh <amir.cheikh@turnkey.io>
2026-01-28 12:42:28 -0500

    Resolved comments

--- packages/react-native-wallet-kit/jest.config.js
+++ packages/react-native-wallet-kit/jest.config.js
@@ -3,7 +3,7 @@ const config = {
   transform: {
     "\\.[jt]sx?$": "@turnkey/jest-config/transformer.js",
   },
-  testMatch: ["**/__tests__/**/*-(spec|test).[jt]s?(x)"],
+  testMatch: ["**/tests/**/*-(spec|test).[jt]s?(x)"],
   testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
   testTimeout: 30 * 1000, // For slow CI machines
 };
