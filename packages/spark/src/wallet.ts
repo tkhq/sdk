982ae3ec73837090a7cb5bc3933bea25f71febed
Jan Nanista <jan@turnkey.io>
2026-06-10 13:58:50 -0700

    Spark support Add support for lightning invoices

--- packages/spark/src/wallet.ts
+++ packages/spark/src/wallet.ts
@@ -1,13 +1,20 @@
 import {
   type ConfigOptions,
+  type CreateLightningInvoiceParams,
   LeafManager,
   SparkSigner,
   SparkWallet,
   SparkWalletEvent,
   SwapService,
 } from "@buildonspark/spark-sdk";
-import { TurnkeyTransferService } from "./services/transfer";
+import {
+  operatorsToOperatorRecipients,
+  TurnkeyTransferService,
+} from "./services/transfer";
 import { TurnkeyCoopExitService } from "./services/coop-exit";
+import type { TurnkeySparkSigner } from "./signer";
+import { uint8ArrayFromHexString } from "@turnkey/encoding";
+import type { LightningReceiveRequest } from "@buildonspark/spark-sdk/dist/types";
 
 export class TurnkeySparkWallet extends SparkWallet {
   constructor(options?: ConfigOptions, signerArg?: SparkSigner) {
@@ -60,6 +67,59 @@ export class TurnkeySparkWallet extends SparkWallet {
     );
   }
 
+  override async createLightningInvoice(
+    params: CreateLightningInvoiceParams,
+  ): Promise<LightningReceiveRequest> {
+    const config = this.config;
+    const signer = this.config.signer as TurnkeySparkSigner;
+    const threshold = config.getThreshold();
+
+    const operatorRecipients = operatorsToOperatorRecipients(
+      config.getSigningOperators(),
+    );
+    const prepared = await signer.prepareLightningReceive({
+      threshold,
+      operatorRecipients,
+    });
+
+    // If storing the encrypted preimage shares fails after invoice creation, rerun
+    // invoice creation instead of reusing the printed invoice; the old hodl invoice
+    // may time out because Spark operators do not have the shares for its hash.
+    const invoice = await this.createLightningHodlInvoice({
+      ...params,
+      paymentHash: prepared.paymentHash,
+    });
+
+    const encryptedPreimageShares: Record<string, Uint8Array> = {};
+    for (const pkg of prepared.operatorPackages) {
+      encryptedPreimageShares[pkg.operatorId] = uint8ArrayFromHexString(
+        pkg.encryptedPackage,
+      );
+    }
+
+    const receiverIdentityPubkey = params.receiverIdentityPubkey
+      ? uint8ArrayFromHexString(params.receiverIdentityPubkey)
+      : await signer.getIdentityPublicKey();
+
+    const sparkClient = await this.connectionManager.createSparkClient(
+      this.config.getCoordinatorAddress(),
+    );
+
+    await sparkClient.store_preimage_share_v2({
+      paymentHash: uint8ArrayFromHexString(prepared.paymentHash),
+      encryptedPreimageShares,
+      threshold,
+      invoiceString: invoice.invoice.encodedInvoice,
+      userIdentityPublicKey: receiverIdentityPubkey,
+    });
+
+    return invoice;
+  }
+
+  // override async payLightningInvoice(params: PayLightningInvoiceParams): Promise<LightningSendRequest | WalletTransfer> {
+
+  // }
+
   /**
    * this.logging is not being exposed by SparkWallet so we'll need to work around TypeScript to get it
    */
@@ -71,3 +131,139 @@ export class TurnkeySparkWallet extends SparkWallet {
     return (this as any).logging as LoggingService | undefined;
   }
 }
+
+// export async function turnkeyPayLightningInvoice(
+//   wallet: SparkWallet,
+//   signer: TurnkeySparkSigner,
+//   params: Omit<PayLightningInvoiceParams, "preferSpark">,
+// ): Promise<unknown> {
+//   const internals = getInternals(wallet);
+//   const config = internals.config;
+//   const invoice = params.invoice.toLowerCase();
+
+//   assertInvoiceNetworkCompatible(invoice, config.getNetworkType());
+
+//   const decodedInvoice = decodeLightningInvoice(invoice);
+//   const isZeroAmountInvoice = !decodedInvoice.amountMSats;
+
+//   if (!isZeroAmountInvoice && params.amountSatsToSend !== undefined) {
+//     throw new SparkValidationError(
+//       "Invalid amount. User can only specify amountSatsToSend for 0 amount lightning invoice",
+//       {
+//         field: "amountMSats",
+//         value: Number(decodedInvoice.amountMSats),
+//         expected: "0",
+//       },
+//     );
+//   }
+
+//   if (isZeroAmountInvoice && params.amountSatsToSend === undefined) {
+//     throw new SparkValidationError(
+//       "Invalid amount. User must specify amountSatsToSend for 0 amount lightning invoice",
+//       {
+//         field: "amountMSats",
+//         value: Number(decodedInvoice.amountMSats),
+//         expected: "0",
+//       },
+//     );
+//   }
+
+//   const amountSats = isZeroAmountInvoice
+//     ? params.amountSatsToSend!
+//     : Math.ceil(Number(decodedInvoice.amountMSats) / 1000);
+
+//   if (isNaN(amountSats) || amountSats <= 0) {
+//     throw new SparkValidationError("Invalid amount", {
+//       field: "amountSats",
+//       value: amountSats,
+//       expected: "greater than 0",
+//     });
+//   }
+
+//   const feeEstimateParams: { encodedInvoice: string; amountSats?: number } = {
+//     encodedInvoice: invoice,
+//   };
+//   if (isZeroAmountInvoice) {
+//     feeEstimateParams.amountSats = params.amountSatsToSend!;
+//   }
+
+//   const feeEstimate =
+//     await wallet.getLightningSendFeeEstimate(feeEstimateParams);
+
+//   if (params.maxFeeSats < feeEstimate) {
+//     throw new SparkValidationError("maxFeeSats does not cover fee estimate", {
+//       field: "maxFeeSats",
+//       value: params.maxFeeSats,
+//       expected: `${feeEstimate} sats`,
+//     });
+//   }
+
+//   const totalAmount = amountSats + feeEstimate;
+//   const sspClient = internals.getSspClient() as unknown as {
+//     requestLightningSend(params: {
+//       encodedInvoice: string;
+//       amountSats?: number;
+//       userOutboundTransferExternalId: string;
+//     }): Promise<unknown>;
+//   };
+//   const paymentHash = fromHex(decodedInvoice.paymentHash);
+//   const expiryTime = new Date(Date.now() + LIGHTNING_SEND_EXPIRY_MS);
+
+//   return internals.leafManager.selectLeavesAndExecute(
+//     [totalAmount],
+//     async (selected) => {
+//       const leaves = selected[0]!;
+//       const sspIdentityPubkey = fromHex(config.getSspIdentityPublicKey());
+//       const leafTweaks = makeLeafTweaks(leaves, sspIdentityPubkey);
+//       const transferId = uuidv7();
+
+//       const startTransferRequest = await prepareTurnkeyTransferForLightning(
+//         internals,
+//         signer,
+//         leafTweaks,
+//         paymentHash,
+//         expiryTime,
+//         transferId,
+//       );
+
+//       const swapResponse =
+//         await internals.lightningService.swapNodesForPreimage({
+//           leaves: leafTweaks,
+//           receiverIdentityPubkey: sspIdentityPubkey,
+//           paymentHash,
+//           isInboundPayment: false,
+//           invoiceString: invoice,
+//           feeSats: feeEstimate,
+//           startTransferRequest,
+//           expiryTime,
+//           transferID: transferId,
+//           ...(params.amountSatsToSend !== undefined
+//             ? { amountSatsToSend: params.amountSatsToSend }
+//             : {}),
+//           ...(params.idempotencyKey !== undefined
+//             ? { idempotencyKey: params.idempotencyKey }
+//             : {}),
+//         });
+
+//       if (!swapResponse.transfer) {
+//         throw new Error("Failed to swap nodes for preimage");
+//       }
+
+//       await internals.leafManager.handleTransferEvent(swapResponse.transfer);
+
+//       const sspResponse = await sspClient.requestLightningSend({
+//         encodedInvoice: invoice,
+//         userOutboundTransferExternalId: swapResponse.transfer.id,
+//         ...(isZeroAmountInvoice
+//           ? { amountSats: params.amountSatsToSend! }
+//           : {}),
+//       });
+
+//       if (!sspResponse) {
+//         throw new Error("Failed to contact SSP");
+//       }
+
+//       return sspResponse;
+//     },
+//   );
+// }
