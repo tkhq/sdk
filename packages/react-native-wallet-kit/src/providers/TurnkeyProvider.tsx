6615ca4bd364c8eb4792b67f3a71805a69f727ce
Rayan Fawaz <rayanfawaz313@gmail.com>
2026-09-25 18:40:27 -0400

    pass walletConfig through to the core client in react-native-wallet-kit

--- packages/react-native-wallet-kit/src/providers/TurnkeyProvider.tsx
+++ packages/react-native-wallet-kit/src/providers/TurnkeyProvider.tsx
@@ -405,6 +405,10 @@ export const TurnkeyProvider: React.FC<TurnkeyProviderProps> = ({
               },
             }
           : {}),
+
+        ...(masterConfig.walletConfig
+          ? { walletConfig: masterConfig.walletConfig }
+          : {}),
       });
 
       await turnkeyClient.init();
